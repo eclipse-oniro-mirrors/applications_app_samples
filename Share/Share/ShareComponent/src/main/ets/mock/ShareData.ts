304b9538a32ec5687dcd9790e7d4358bafe2a7b7
wjxCode <wangjixing2@huawei.com>
2022-09-02 09:46:17 +0800

    修改引入中的大小写问题
    Signed-off-by: wjxCode <wangjixing2@huawei.com>

--- Share/Share/ShareComponent/src/main/ets/mock/ShareData.ts
+++ Share/Share/ShareComponent/src/main/ets/mock/ShareData.ts
@@ -13,7 +13,7 @@
  * limitations under the License.
  */
 
-import { DialogDataModel } from "../model/shareModel"
+import { DialogDataModel } from "../model/ShareModel"
 
 export const dialogData: Array<DialogDataModel> = [
   {
