01578e705d538d40b3d970c1127a62bf56d3fca7
zhongyuyan <zhongyuyan1@huawei.com>
2022-09-08 10:55:48 +0800

    修复自动化编译问题
    Signed-off-by: zhongyuyan <zhongyuyan1@huawei.com>

--- ability/DataAbility/entry/src/ohosTest/ets/TestRunner/OpenHarmonyTestRunner.ts
+++ ability/DataAbility/entry/src/ohosTest/ets/TestRunner/OpenHarmonyTestRunner.ts
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import TestRunner from '@ohos.application.testRunner'
 import AbilityDelegatorRegistry from '@ohos.application.abilityDelegatorRegistry'
 
@@ -8,7 +23,7 @@ function translateParamsToString(parameters) {
   const keySet = new Set([
     '-s class', '-s notClass', '-s suite', '-s itName',
     '-s level', '-s testType', '-s size', '-s timeout',
-    '-s package'
+    '-s package', '-s dryRun'
   ])
   let targetParams = ''
   for (const key in parameters) {
@@ -20,7 +35,7 @@ function translateParamsToString(parameters) {
 }
 
 async function onAbilityCreateCallback() {
-  console.log('onAbilityCreateCallback')
+  console.log('onAbilityCreateCallback');
 }
 
 async function addAbilityMonitorCallback(err: any) {
@@ -48,6 +63,10 @@ export default class OpenHarmonyTestRunner implements TestRunner {
     abilityDelegator.addAbilityMonitor(lMonitor, addAbilityMonitorCallback)
     var cmd = 'aa start -d 0 -a ' + testAbilityName + ' -b ' + abilityDelegatorArguments.bundleName
     cmd += ' ' + translateParamsToString(abilityDelegatorArguments.parameters)
+    var debug = abilityDelegatorArguments.parameters["-D"]
+    if (debug == 'true') {
+      cmd += ' -D'
+    }
     console.info('cmd : ' + cmd)
     abilityDelegator.executeShellCommand(cmd,
       (err: any, d: any) => {
