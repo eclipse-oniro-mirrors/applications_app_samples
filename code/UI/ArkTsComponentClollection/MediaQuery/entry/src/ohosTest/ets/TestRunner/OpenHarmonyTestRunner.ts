873e4b9da18b018194351f5c6a976e2052fd82f8
mashitao2 <mashitao2@huawei.com>
2023-02-13 11:02:24 +0800

    IssueNo: #I6A7HO:MediaQuery升级Stage模型 Description: MediaQuery Upgrade Stage Model Sig: SIG_applications_app_samples Feature or Bugfix: Feature Binary Source: No Signed-off-by: mashitao2 <mashitao2@huawei.com>

--- code/UI/ArkTsComponentClollection/MediaQuery/entry/src/ohosTest/ets/TestRunner/OpenHarmonyTestRunner.ts
+++ code/UI/ArkTsComponentClollection/MediaQuery/entry/src/ohosTest/ets/TestRunner/OpenHarmonyTestRunner.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2022-2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -15,31 +15,33 @@
 
 import TestRunner from '@ohos.application.testRunner'
 import AbilityDelegatorRegistry from '@ohos.application.abilityDelegatorRegistry'
+import Logger from '../../../main/ets/model/Logger'
 
 var abilityDelegator = undefined
 var abilityDelegatorArguments = undefined
+const TAG = 'OpenHarmonyTestRunner'
 
 function translateParamsToString(parameters) {
   const keySet = new Set([
-    '-s class', '-s notClass', '-s suite', '-s itName',
+    '-s class', '-s notClass', '-s suite', '-s it',
     '-s level', '-s testType', '-s size', '-s timeout',
-    '-s package', '-s dryRun'
+    '-s dryRun'
   ])
-  let targetParams = ''
+  let targetParams = '';
   for (const key in parameters) {
     if (keySet.has(key)) {
-      targetParams += ' ' + key + ' ' + parameters[key]
+      targetParams = `${targetParams} ${key} ${parameters[key]}`
     }
   }
   return targetParams.trim()
 }
 
 async function onAbilityCreateCallback() {
-  console.log('onAbilityCreateCallback')
+  Logger.info(TAG, 'onAbilityCreateCallback');
 }
 
 async function addAbilityMonitorCallback(err: any) {
-  console.info('addAbilityMonitorCallback : ' + JSON.stringify(err))
+  Logger.info(TAG, 'addAbilityMonitorCallback :', JSON.stringify(err) ?? '');
 }
 
 export default class OpenHarmonyTestRunner implements TestRunner {
@@ -47,36 +49,32 @@ export default class OpenHarmonyTestRunner implements TestRunner {
   }
 
   onPrepare() {
-    console.info('OpenHarmonyTestRunner OnPrepare')
+    Logger.info(TAG, 'OpenHarmonyTestRunner OnPrepare ');
   }
 
-  onRun() {
-    console.log('OpenHarmonyTestRunner onRun run')
+  async onRun() {
+    Logger.info(TAG, 'OpenHarmonyTestRunner onRun run');
     abilityDelegatorArguments = AbilityDelegatorRegistry.getArguments()
     abilityDelegator = AbilityDelegatorRegistry.getAbilityDelegator()
-
+    var testAbilityName = abilityDelegatorArguments.bundleName + '.TestAbility'
     let lMonitor = {
       abilityName: testAbilityName,
       onAbilityCreate: onAbilityCreateCallback,
     };
-    var testAbilityName = abilityDelegatorArguments.parameters['-p'] + '.TestAbility'
     abilityDelegator.addAbilityMonitor(lMonitor, addAbilityMonitorCallback)
-    var cmd = 'aa start -d 0 -a ' + testAbilityName + ' -b ' + abilityDelegatorArguments.bundleName
+    var cmd = 'aa start -d 0 -a TestAbility' + ' -b ' + abilityDelegatorArguments.bundleName
     cmd += ' ' + translateParamsToString(abilityDelegatorArguments.parameters)
-    var debug = abilityDelegatorArguments.parameters["-D"]
+    var debug = abilityDelegatorArguments.parameters['-D']
     if (debug == 'true') {
       cmd += ' -D'
     }
-    console.info('cmd : ' + cmd)
+    Logger.info(TAG, 'cmd :', cmd);
     abilityDelegator.executeShellCommand(cmd,
       (err: any, d: any) => {
-        console.info('executeShellCommand : err : ' + JSON.stringify(err))
-        console.info('executeShellCommand : data : ' + d.stdResult)
-        console.info('executeShellCommand : data : ' + d.exitCode)
+        Logger.info(TAG, JSON.stringify(err) ?? '');
+        Logger.info(TAG, d.stdResult ?? '');
+        Logger.info(TAG, d.exitCode ?? '');
       })
-    console.info('OpenHarmonyTestRunner onRun call abilityDelegator.getAppContext')
-    var context = abilityDelegator.getAppContext()
-    console.info('getAppContext : ' + JSON.stringify(context))
-    console.info('OpenHarmonyTestRunner onRun end')
+    Logger.info(TAG, 'OpenHarmonyTestRunner onRun end');
   }
 }
