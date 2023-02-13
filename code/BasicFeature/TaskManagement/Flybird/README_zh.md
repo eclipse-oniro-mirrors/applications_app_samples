b10d06195968bed03d5a8c7682e9c69d79eaeb10
mashitao2 <mashitao2@huawei.com>
2023-02-13 11:26:33 +0800

    IssueNo: #I69ZTR:Flybird升级Stage模型 Description: Flybird Upgrade Stage Model Sig: SIG_applications_app_samples Feature or Bugfix: Feature Binary Source: No Signed-off-by: mashitao2 <mashitao2@huawei.com>

--- code/BasicFeature/TaskManagement/Flybird/README_zh.md
+++ code/BasicFeature/TaskManagement/Flybird/README_zh.md
@@ -1,28 +1,8 @@
 # 小鸟避障游戏
 
-### 简介
+### 介绍
 
-本示例用于验证OpenHarmony提供的FA模型，Canvas组件做小游戏的能效是否符合预期，以及验证资源调度子系统的API接口是否符合后台规范运行的预期。实现效果如下：
-
-![](screenshot/snapshot1.png) ![](screenshot/snapshot2.png)
-
-### 相关概念
-
-Canvas：提供画布组件，用于自定义绘制图形。
-
-### 相关权限
-
-后台持续运行: ohos.permission.KEEP_BACKGROUND_RUNNING
-
-查询其他应用的运行时间: ohos.permission.BUNDLE_ACTIVE_INFO
-
-读取用户存储: ohos.permission.READ_USER_STORAGE
-
-写入用户存储: ohos.permission.WRITE_USER_STORAGE
-
-后台代理提醒: ohos.permission.PUBLISH_AGENT_REMINDER
-
-### 使用说明
+本示例用于验证OpenHarmony提供的Stage模型，Canvas组件做小游戏的能效是否符合预期，以及验证资源调度子系统的API接口是否符合后台规范运行的预期。具体操作如下：
 
 1.点击**ConnectService**按钮，创建一个ServiceAbility并连接。
 
@@ -36,12 +16,32 @@ Canvas：提供画布组件，用于自定义绘制图形。
 
 6.点**home**按钮后，应用生命周期为onInactive, 在此回调中起一个短时任务，验证短时任务API能否正常执行。
 
+### 效果预览
+
+![](screenshot/snapshot1.png) ![](screenshot/snapshot2.png)
+
+### 相关权限
+
+后台持续运行: [ohos.permission.KEEP_BACKGROUND_RUNNING](https://gitee.com/openharmony/docs/blob/master/zh-cn/application-dev/security/permission-list.md)
+
+查询其他应用的运行时间: [ohos.permission.BUNDLE_ACTIVE_INFO](https://gitee.com/openharmony/docs/blob/master/zh-cn/application-dev/security/permission-list.md)
+
+读取用户存储: [ohos.permission.READ_USER_STORAGE](https://gitee.com/openharmony/docs/blob/master/zh-cn/application-dev/security/permission-list.md)
+
+写入用户存储: [ohos.permission.WRITE_USER_STORAGE](https://gitee.com/openharmony/docs/blob/master/zh-cn/application-dev/security/permission-list.md)
+
+后台代理提醒: [ohos.permission.PUBLISH_AGENT_REMINDER](https://gitee.com/openharmony/docs/blob/master/zh-cn/application-dev/security/permission-list.md)
+
+### 依赖
+
+不涉及。
+
 ### 约束与限制
 
 1.本示例仅支持在标准系统上运行。
 
-2.从API version 9开始支持。
+2.本示例仅支持API9版本SDK，版本号：3.2.9.2。
 
-3.本示例需要使用DevEco Studio 3.0 Beta4 (Build Version: 3.0.0.992, built on July 14, 2022)才可编译运行。
+3.本示例需要使用DevEco Studio 3.1 Canary1 (Build Version: 3.1.0.100, built on November 3, 2022)才可编译运行。
 
-4.本示例所配置的权限ohos.permission.BUNDLE_ACTIVE_INFO为system_basic级别(相关权限级别可通过[权限定义列表](https://gitee.com/openharmony/docs/blob/master/zh-cn/application-dev/security/permission-list.md)查看)，需要手动配置对应级别的权限签名(具体操作可查看[自动化签名方案](https://docs.openharmony.cn/pages/v3.2Beta/zh-cn/application-dev/security/hapsigntool-overview.md/))。
+4.本示例所配置的权限ohos.permission.BUNDLE_ACTIVE_INFO为system_basic级别(相关权限级别可通过[权限定义列表](https://gitee.com/openharmony/docs/blob/master/zh-cn/application-dev/security/permission-list.md)查看) ，需要手动配置对应级别的权限签名(具体操作可查看[自动化签名方案](https://docs.openharmony.cn/pages/v3.2Beta/zh-cn/application-dev/security/hapsigntool-overview.md/)) 。
--- code/BasicFeature/TaskManagement/Flybird/entry/src/main/ets/ServiceAbility/service.ts
+++ code/BasicFeature/TaskManagement/Flybird/entry/src/main/ets/ServiceAbility/service.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2022-2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -14,87 +14,89 @@
  */
 
 import rpc from "@ohos.rpc"
-import wantAgent from '@ohos.wantAgent';
-import backgroundTaskManager from '@ohos.backgroundTaskManager';
-import featureAbility from '@ohos.ability.featureAbility';
-import logger from '../MainAbility/utils/logger'
+import wantAgent from '@ohos.wantAgent'
+import ServiceExtensionAbility from '@ohos.application.ServiceExtensionAbility'
+import Logger from '../model/Logger'
 
-const TAG: string = '[flybirdDebug.ServiceAbility]'
+const TAG: string = 'ServiceAbility'
 
 class FirstServiceAbilityStub extends rpc.RemoteObject {
-    constructor(des: any) {
-        if (typeof des === 'string') {
-            super(des)
-        } else {
-            return
+  constructor(des: string) {
+    if (typeof des === 'string') {
+      super(des)
+    } else {
+      return
+    }
+  }
+
+  onRemoteRequest(code: number, data: rpc.MessageParcel, reply: rpc.MessageParcel, option: rpc.MessageOption) {
+    Logger.info(`${TAG}onRemoteRequest called`)
+    if (code === 1) {
+      let dataStr = data.readString()
+      Logger.info(`${TAG} string=${dataStr}`)
+      if (dataStr === 'start_game') {
+        this.startContinousTask();
+        let result = 'ok start game'
+        Logger.info(`${TAG} result=${result}`)
+        reply.writeString(result)
+      } else if (dataStr === 'disconnect_service') {
+        let result = 'ok disconnect service'
+        Logger.info(`${TAG} result=${result}`)
+        reply.writeString(result)
+      }
+      else {
+        Logger.info(`${TAG} error string}`)
+      }
+    } else {
+      Logger.info(`${TAG} unknown request code`)
+    }
+    return true
+  }
+
+  startContinousTask() {
+    Logger.info(TAG, 'start background continousTask api')
+    let wantAgentInfo = {
+      wants: [
+        {
+          bundleName: 'ohos.samples.flybird',
+          abilityName: 'EntryAbility'
         }
+      ],
+      operationType: wantAgent.OperationType.START_ABILITY,
+      requestCode: 0,
+      wantAgentFlags: [wantAgent.WantAgentFlags.UPDATE_PRESENT_FLAG]
+    };
+
+  }
+
+  callback(err, data) {
+    if (err) {
+      Logger.error(TAG, `Operation failed Cause: ${err}`)
+    } else {
+      Logger.info(TAG, "Operation succeeded")
     }
-
-    onRemoteRequest(code: number, data: any, reply: any, option: any) {
-        logger.log(`${TAG}onRemoteRequest called`)
-        if (code === 1) {
-            let dataStr = data.readString()
-            logger.log(`${TAG} string=${dataStr}`)
-            if (dataStr === 'start_game') {
-                this.startContinousTask();
-                let result = 'ok start game'
-                logger.log(`${TAG} result=${result}`)
-                reply.writeString(result)
-            } else if (dataStr === 'disconnect_service') {
-                let result = 'ok disconnect service'
-                logger.log(`${TAG} result=${result}`)
-                reply.writeString(result)
-            } 
-            else {
-                logger.log(`${TAG} error string}`)
-            }
-        } else {
-            logger.log(`${TAG} unknown request code`)
-        }
-        return true;
-    }
-
-    startContinousTask() {
-        logger.info(TAG + " start background continousTask api");
-
-        let wantAgentInfo = {
-            wants: [
-                {
-                    bundleName: "com.samples.flybird",
-                    abilityName: "com.samples.flybird.MainAbility"
-                }
-            ],
-            operationType:wantAgent.OperationType.START_ABILITY,
-            requestCode: 0,
-            wantAgentFlags: [wantAgent.WantAgentFlags.UPDATE_PRESENT_FLAG]
-        };
-
-    }
-     callback(err, data) {
-        if (err) {
-            logger.error(TAG + "Operation failed Cause: " + err);
-        } else {
-            logger.info(TAG + "Operation succeeded");
-        }
-    }
-
+  }
 }
 
-export default {
-    onStart() {
-        logger.info(`${TAG} onStart`)
-    },
-    onStop() {
-        logger.info(`${TAG} onStop`)
-    },
-    onConnect(want) {
-        logger.log(`${TAG} onConnect, want:${JSON.stringify(want)}`)
-        return new FirstServiceAbilityStub("first ts service stub")
-    },
-    onDisconnect(want) {
-        logger.log(`${TAG} onDisconnect, want:${JSON.stringify(want)}`)
-    },
-    onCommand(want, startId) {
-        logger.log(`${TAG} onCommand, want:${JSON.stringify(want)},startId:${startId}`)
-    }
-};
+export default class ServiceExtAbility extends ServiceExtensionAbility {
+  onCreate(want) {
+    Logger.info(`${TAG} onCreate, want: ${want.abilityName}`)
+  }
+
+  onRequest(want, startId) {
+    Logger.info(`${TAG} onRequest, want: ${want.abilityName}`)
+  }
+
+  onConnect(want) {
+    Logger.info(`${TAG} onConnect, want: ${want.abilityName}`)
+    return new FirstServiceAbilityStub("first ts service stub")
+  }
+
+  onDisconnect(want) {
+    Logger.info(`${TAG} onDisconnect, want: ${want.abilityName}`)
+  }
+
+  onDestroy() {
+    Logger.info(`${TAG} onDestroy`)
+  }
+}
