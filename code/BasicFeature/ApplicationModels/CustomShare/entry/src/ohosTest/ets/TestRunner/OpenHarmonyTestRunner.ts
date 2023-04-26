0f2fb6e18ddec3163a91d76430b9e26899146849
zwx1094577 <zhongyuyan1@huawei.com>
2023-04-26 19:16:35 +0800

    IssueNo: #I6YVD3:【Sample】新增CustomShare，MusicAlbum自动化测用例 Description: Add CustomShare and MusicAlbum test cases. Sig: SIG_applications_app_samples Feature or Bugfix: Feature Binary Source: No
    Signed-off-by: zwx1094577 <zhongyuyan1@huawei.com>

--- code/BasicFeature/ApplicationModels/CustomShare/entry/src/ohosTest/ets/TestRunner/OpenHarmonyTestRunner.ts
+++ code/BasicFeature/ApplicationModels/CustomShare/entry/src/ohosTest/ets/TestRunner/OpenHarmonyTestRunner.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2022-2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -12,35 +12,36 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import TestRunner from '@ohos.application.testRunner'
-import AbilityDelegatorRegistry from '@ohos.application.abilityDelegatorRegistry'
-import Logger from '../../../main/ets/model/Logger'
 
-let abilityDelegator = undefined
-let abilityDelegatorArguments = undefined
-const TAG: string = 'OpenHarmonyTestRunner'
+import TestRunner from '@ohos.application.testRunner';
+import AbilityDelegatorRegistry from '@ohos.application.abilityDelegatorRegistry';
+import Logger from '../../../main/ets/model/Logger';
+
+let abilityDelegator = undefined;
+let abilityDelegatorArguments = undefined;
+const TAG: string = 'OpenHarmonyTestRunner';
 
 function translateParamsToString(parameters) {
   const keySet = new Set([
     '-s class', '-s notClass', '-s suite', '-s it',
     '-s level', '-s testType', '-s size', '-s timeout',
     '-s dryRun'
-  ])
+  ]);
   let targetParams = '';
   for (const key in parameters) {
     if (keySet.has(key)) {
-      targetParams = `${targetParams} ${key} ${parameters[key]}`
+      targetParams = `${targetParams} ${key} ${parameters[key]}`;
     }
   }
-  return targetParams.trim()
+  return targetParams.trim();
 }
 
 async function onAbilityCreateCallback() {
-  Logger.info(TAG, `onAbilityCreateCallback`)
+  Logger.info(TAG, 'onAbilityCreateCallback');
 }
 
 async function addAbilityMonitorCallback(err: any) {
-  Logger.info(TAG, `addAbilityMonitorCallback : ${JSON.stringify(err)}`)
+  Logger.info(TAG, `addAbilityMonitorCallback : ${JSON.stringify(err)}`);
 }
 
 export default class OpenHarmonyTestRunner implements TestRunner {
@@ -48,32 +49,32 @@ export default class OpenHarmonyTestRunner implements TestRunner {
   }
 
   onPrepare() {
-    Logger.info(TAG, `OpenHarmonyTestRunner OnPrepare `)
+    Logger.info(TAG, 'OpenHarmonyTestRunner OnPrepare');
   }
 
   async onRun() {
-    Logger.info(TAG, `OpenHarmonyTestRunner onRun run`)
-    abilityDelegatorArguments = AbilityDelegatorRegistry.getArguments()
-    abilityDelegator = AbilityDelegatorRegistry.getAbilityDelegator()
-    let testAbilityName = abilityDelegatorArguments.bundleName + '.TestAbility'
+    Logger.info(TAG, 'OpenHarmonyTestRunner onRun run');
+    abilityDelegatorArguments = AbilityDelegatorRegistry.getArguments();
+    abilityDelegator = AbilityDelegatorRegistry.getAbilityDelegator();
+    let testAbilityName = abilityDelegatorArguments.bundleName + '.TestAbility';
     let lMonitor = {
       abilityName: testAbilityName,
       onAbilityCreate: onAbilityCreateCallback,
-    }
-    abilityDelegator.addAbilityMonitor(lMonitor, addAbilityMonitorCallback)
-    let cmd = 'aa start -d 0 -a TestAbility' + ' -b ' + abilityDelegatorArguments.bundleName
-    cmd += ' ' + translateParamsToString(abilityDelegatorArguments.parameters)
-    let debug = abilityDelegatorArguments.parameters["-D"]
+    };
+    abilityDelegator.addAbilityMonitor(lMonitor, addAbilityMonitorCallback);
+    let cmd = 'aa start -d 0 -a TestAbility' + ' -b ' + abilityDelegatorArguments.bundleName;
+    cmd += ' ' + translateParamsToString(abilityDelegatorArguments.parameters);
+    let debug = abilityDelegatorArguments.parameters['-D'];
     if (debug == 'true') {
-      cmd += ' -D'
+      cmd += ' -D';
     }
-    Logger.info(TAG, `cmd : ${cmd}`)
+    Logger.info(TAG, `cmd : ${cmd}`);
     abilityDelegator.executeShellCommand(cmd,
       (err: any, d: any) => {
-        Logger.info(TAG, `executeShellCommand : err : ${JSON.stringify(err)}`)
-        Logger.info(TAG, 'executeShellCommand : data : ${d.stdResult}')
-        Logger.info(TAG, 'executeShellCommand : data : ${d.exitCode}')
+        Logger.info(TAG, `executeShellCommand : err : ${JSON.stringify(err)}`);
+        Logger.info(TAG, 'executeShellCommand : data : ${d.stdResult}');
+        Logger.info(TAG, 'executeShellCommand : data : ${d.exitCode}');
       })
-    Logger.info(TAG, `OpenHarmonyTestRunner onRun end`)
+    Logger.info(TAG, 'OpenHarmonyTestRunner onRun end');
   }
 }
