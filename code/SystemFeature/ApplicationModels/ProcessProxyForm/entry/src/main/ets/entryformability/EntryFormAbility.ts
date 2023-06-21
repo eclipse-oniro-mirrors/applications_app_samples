7724c1e39d6d64897fa7bcb4d3e463cf61aff801
xinking129 <xinxin13@huawei.com>
2023-06-21 16:54:40 +0800

    add ProcessProxyForm
    Signed-off-by: xinking129 <xinxin13@huawei.com>

---
+++ code/SystemFeature/ApplicationModels/ProcessProxyForm/entry/src/main/ets/entryformability/EntryFormAbility.ts
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import formInfo from '@ohos.app.form.formInfo';
+import formBindingData from '@ohos.app.form.formBindingData';
+import FormExtensionAbility from '@ohos.app.form.FormExtensionAbility';
+import Logger from '../../common/Logger';
+
+const TAG = '[EntryFormAbility]';
+
+export default class EntryFormAbility extends FormExtensionAbility {
+  onAddForm(want) {
+    Logger.info(`${TAG}`, `onAddForm want: ${JSON.stringify(want)}`);
+    let formData = {
+      'city_name': '沈阳',
+      'city_temper': -30
+    };
+    let proxies = [
+      {
+        'key': 'city_name',
+        'subscriberId': '110000'
+      },
+      {
+        'key': 'city_temper',
+        'subscriberId': '110000'
+      }
+    ];
+    let formBinding = formBindingData.createFormBindingData(formData);
+    formBinding['proxies'] = proxies;
+    Logger.info(`${TAG}`, `onAddForm return formBinding: ${JSON.stringify(formBinding)}`);
+    return formBinding;
+  }
+
+  onCastToNormalForm(formId) {
+    Logger.info(`${TAG}`, `onCastToNormalForm formId: ${JSON.stringify(formId)}`);
+  }
+
+  onUpdateForm(formId) {
+    Logger.info(`${TAG}`, `onUpdateForm formId: ${JSON.stringify(formId)}`);
+  }
+
+  onChangeFormVisibility(newStatus) {
+    Logger.info(`${TAG}`, `onChangeFormVisibility newStatus: ${JSON.stringify(newStatus)}`);
+  }
+
+  onFormEvent(formId, message) {
+    Logger.info(`${TAG}`, `onFormEvent formId: ${JSON.stringify(formId)}`, `message: ${JSON.stringify(message)}`);
+  }
+
+  onRemoveForm(formId) {
+    Logger.info(`${TAG}`, `onRemoveForm formId: ${JSON.stringify(formId)}`);
+  }
+
+  onAcquireFormState(want) {
+    Logger.info(`${TAG}`, `onAcquireFormState want: ${JSON.stringify(want)}`);
+    return formInfo.FormState.READY;
+  }
+};
