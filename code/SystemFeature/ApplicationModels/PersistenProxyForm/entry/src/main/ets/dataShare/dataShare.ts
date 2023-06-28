69f7e4ce40819bf4fbb59545bf08a623c1ecbf2c
xinking129 <xinxin13@huawei.com>
2023-06-28 10:17:54 +0800

    changeName
    Signed-off-by: xinking129 <xinxin13@huawei.com>

--- code/SystemFeature/ApplicationModels/PersistenProxyForm/entry/src/main/ets/dataShare/dataShare.ts
+++ code/SystemFeature/ApplicationModels/PersistenProxyForm/entry/src/main/ets/dataShare/dataShare.ts
@@ -25,50 +25,50 @@ let rdbStore;
 const TAG = '[dataShare]';
 
 export default class DataShareExtAbility extends DataShareExtensionAbility {
-    async onCreate(want, callback) {
-        Logger.info(`${TAG}`, `DataShareExtAbility onCreate this.context.databaseDir: ${this.context.databaseDir}`);
-        // @ts-ignore
-        rdbStore = await rdb.getRdbStore(this.context, { name: DB_NAME, securityLevel: rdb.SecurityLevel.S1 });
-        Logger.info(`${TAG}`, 'DataShareExtAbility getRdbStore done.');
-        await rdbStore.executeSql(DDL_TBL_CREATE, []);
-        Logger.info(`${TAG}`, 'DataShareExtAbility executeSql multiple tables done.');
-        let err = {'code':0};
-        callback(err);
-        let rdata = {
-            'cityId': 310000,
-            'cityName': '杭州'
-        };
-        let rdata2 = {
-            'cityId': 110000,
-            'cityName': '沈阳'
-        };
-        await rdbStore.insert(TBL_NAME, rdata, function (err, ret) {
-            Logger.info(`${TAG}`, `[insert] callback ret: ${JSON.stringify(ret)}`);
-        });
-        await rdbStore.insert(TBL_NAME, rdata2, function (err, ret) {
-            Logger.info(`${TAG}`, `[insert] callback ret: ${JSON.stringify(ret)}`);
-        });
-    }
+  async onCreate(want, callback) {
+    Logger.info(`${TAG}`, `DataShareExtAbility onCreate this.context.databaseDir: ${this.context.databaseDir}`);
+    // @ts-ignore
+    rdbStore = await rdb.getRdbStore(this.context, { name: DB_NAME, securityLevel: rdb.SecurityLevel.S1 });
+    Logger.info(`${TAG}`, 'DataShareExtAbility getRdbStore done.');
+    await rdbStore.executeSql(DDL_TBL_CREATE, []);
+    Logger.info(`${TAG}`, 'DataShareExtAbility executeSql multiple tables done.');
+    let err = {'code':0};
+    callback(err);
+    let rdata = {
+      'cityId': 310000,
+      'cityName': '杭州'
+    };
+    let rdata2 = {
+      'cityId': 110000,
+      'cityName': '沈阳'
+    };
+    await rdbStore.insert(TBL_NAME, rdata, function (err, ret) {
+      Logger.info(`${TAG}`, `[insert] callback ret: ${JSON.stringify(ret)}`);
+    });
+    await rdbStore.insert(TBL_NAME, rdata2, function (err, ret) {
+      Logger.info(`${TAG}`, `[insert] callback ret: ${JSON.stringify(ret)}`);
+    });
+  }
 
-    async update(uri, predicates, value, callback) {
-        console.info('[ttt] [DataShareTest] <<Provider>> [update] enter');
-        if (predicates === null || predicates === undefined) {
-            console.info('[ttt] [DataShareTest] <<Provider>> [update] invalid predicates');
-            return;
-        }
-        Logger.info(`${TAG}`, `[update]  values = ${value}`);
-        Logger.info(`${TAG}`, `[update]  [update]  predicates = ${predicates}`);
-        try {
-            await rdbStore.update(TBL_NAME, value, predicates, function (err, ret) {
-                Logger.info(`${TAG}`, `[update] callback ret: ${ret}`);
-                Logger.info(`${TAG}`, `[update] callback err: ${err}`);
-                if (callback !== undefined) {
-                    callback(err, ret);
-                }
-            });
-        } catch (err) {
-            Logger.error(`${TAG}`, `[update] error: ${err}`);
-        };
-        Logger.info(`${TAG}`, '[update] leave');
+  async update(uri, predicates, value, callback) {
+    console.info('[ttt] [DataShareTest] <<Provider>> [update] enter');
+    if (predicates === null || predicates === undefined) {
+      console.info('[ttt] [DataShareTest] <<Provider>> [update] invalid predicates');
+      return;
     }
+    Logger.info(`${TAG}`, `[update]  values = ${value}`);
+    Logger.info(`${TAG}`, `[update]  [update]  predicates = ${predicates}`);
+    try {
+      await rdbStore.update(TBL_NAME, value, predicates, function (err, ret) {
+        Logger.info(`${TAG}`, `[update] callback ret: ${ret}`);
+        Logger.info(`${TAG}`, `[update] callback err: ${err}`);
+        if (callback !== undefined) {
+          callback(err, ret);
+        }
+      });
+    } catch (err) {
+      Logger.error(`${TAG}`, `[update] error: ${err}`);
+    };
+      Logger.info(`${TAG}`, '[update] leave');
+  }
 };
