df502e30ade13aa0ddde83ab12e40453e4c23143
wangqingkaihong <wangqing@kaihong.com>
2023-07-05 14:44:51 +0800

    新版本IDE备份适配修改
    Signed-off-by: wangqingkaihong <wangqing@kaihong.com>

---
+++ code/BasicFeature/FileManagement/FileBackupExtension/entry/src/main/ets/common/BackupExtension.ts
@@ -0,0 +1,12 @@
+// import Extesnion from '@ohos.application.BackupExtensionAbility';
+
+
+export default class BackupExtension {
+  onBackup(): void {
+    console.info('onBackup');
+  }
+  // onRestore(bundleVersion: BundleVersion): void
+  // {
+  //     console.info('onRestore');
+  // }
+}
