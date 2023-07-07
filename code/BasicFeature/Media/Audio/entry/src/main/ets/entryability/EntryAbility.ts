5560bfb1cb3d4a115daf4bc82936e392e870c021
yangliping <yangliping25@huawei.com>
2023-07-07 15:32:10 +0800

    Audio Capturer add
    Signed-off-by: yangliping <yangliping25@huawei.com>

--- code/BasicFeature/Media/Audio/entry/src/main/ets/entryability/EntryAbility.ts
+++ code/BasicFeature/Media/Audio/entry/src/main/ets/entryability/EntryAbility.ts
@@ -1,6 +1,7 @@
 import UIAbility from '@ohos.app.ability.UIAbility';
 import hilog from '@ohos.hilog';
 import window from '@ohos.window';
+import abilityAccessCtrl from '@ohos.abilityAccessCtrl';
 export default class EntryAbility extends UIAbility {
   onCreate(want, launchParam) {
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onCreate');
@@ -13,6 +14,14 @@ export default class EntryAbility extends UIAbility {
   onWindowStageCreate(windowStage: window.WindowStage) {
     // Main window is created, set main page for this ability
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onWindowStageCreate');
+    let atManager = abilityAccessCtrl.createAtManager();
+    atManager.requestPermissionsFromUser(this.context, ['ohos.permission.MICROPHONE']).then((data) => {
+      console.info('data:' + JSON.stringify(data));
+      console.info('data permissions:' + data.permissions);
+      console.info('data authResults:' + data.authResults);
+    }).catch((err) => {
+      console.info('data:' + JSON.stringify(err));
+    });
     windowStage.loadContent('pages/Index', (err, data) => {
       if (err.code) {
         hilog.error(0x0000, 'testTag', 'Failed to load the content. Cause: %{public}s', JSON.stringify(err) ?? '');
