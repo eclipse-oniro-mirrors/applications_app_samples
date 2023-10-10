100755ec146e08861395979ec73607f0be009e02
xujie <xujie223@huawei.com>
2023-10-10 14:09:49 +0800

    vpn sample
    Signed-off-by: xujie <xujie223@huawei.com>

---
+++ code/BasicFeature/Connectivity/VPN/README_zh.md
@@ -0,0 +1,60 @@
+# VPN连接
+
+**介绍**
+本示例使用@ohos.net.vpn接口创建VPN 隧道，建立vpn网络，vpn隧道保护，销毁VPN网络等功能。
+实现了VPN的进行服务器连接，发送数据的功能
+**预览效果:**
+ ![0.jpeg](sceenshots%2F0.jpeg)   
+![1.jpeg](sceenshots%2F1.jpeg)
+![2.jpeg](sceenshots%2F2.jpeg)
+**使用说明**
+
+1.搭建服务器环境，服务端配置
+
+`iptables -I FORWARD -j ACCEPT`
+在iptables防火墙的FORWARD链中添加规则，允许网络数据包的转发。
+
+`echo 1 > /proc/sys/net/ipv4/ip_forward`
+启用IP转发
+
+`iptables -t nat -A POSTROUTING -s 10.0.0.0/24 -o eho1 -j MASQUERADE`
+命令用于配置iptables防火墙的网络地址转换（NAT）规则
+
+`sudo ./server`
+创建服务器
+
+2.运行服务器后会有创建tun成功提示
+
+3.打开vpn应用。用户点击创建对象，隧道保护，启动vpn等按钮，vpn启动后尝试用别的hap包访问百度
+
+4.访问成功后返回选在关闭vpn按钮，断开vpn连接
+
+**工程目录**
+
+entry/src/main/ets/
+|---entryability
+|   |---EntryAbility.ts            // 项目入口，请求相关权限
+|---pages
+|   |---Index.ets                  // 首页
+|   |---StartVpn.ets               // 打开vpn
+|   |---StopVpn.ets                // 关闭vpn
+|---model
+|   |---Logger.ets                 // 日志
+
+**相关权限**
+网络权限: ohos.permission.INTERNET
+VPN权限: ohos.permission.MANAGE_VPN
+
+
+**约束与限制**
+本示例仅支持标准系统上运行，支持设备：RK3568。
+
+本示例为Stage模型，已适配API version 10版本SDK，版本号：4.0.9.6，镜像版本号: Openharmony 4.0.9.6。
+
+本示例需要使用DevEco Studio 3.1 Release (Build Version: 3.1.0.500, built on April 28, 2023)才可编译运行。
+
+本示例在启动前需搭建服务端环境，成功启动相应服务端后再运行客户端，服务端脚本（server_python）需要在Python 3.8.5版本下运行（需与客户端处于同一局域网）。
+
+该示例运行测试完成后，再次运行需要重新启动服务端和客户端
+
+**下载本工程**
---
+++ code/BasicFeature/Connectivity/VPN/server/udp_server.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <thread>
+#include <cstring>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <thread>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include "../common.h"
+
+
+#define PORT 8888
+#define BUFFER_SIZE 2048
+
+extern void WriteToTunfd(void* buffer, int32_t len);
+extern int ReadFromTunfd(void* buffer, int32_t len);
+
+struct ClientInfo {
+    int sockfd = 0;
+};
+
+static bool threadRunF;
+struct sockaddr_in client_address;
+
+void handle_connection(ClientInfo clientInfo) {
+    int addrlen = sizeof(struct sockaddr_in);
+    uint8_t buffer[BUFFER_SIZE] = {0};
+    while (threadRunF) {
+        int length = recvfrom(clientInfo.sockfd, buffer, sizeof(buffer), 0, (struct sockaddr *)&client_address, (socklen_t*)&addrlen);
+        if (length <= 0) {
+            logger_error("Failed to recvfrom data");
+            continue;
+        }
+
+        logger_info("from [%s:%d] data: %s, len: %d", inet_ntoa(client_address.sin_addr), ntohs(client_address.sin_port), buffer, length);
+        WriteToTunfd(buffer, length);
+    }
+}
+
+void HandleTunReceived(ClientInfo clientInfo) {
+    char buffer[BUFFER_SIZE] = {0};
+    int addrlen = sizeof(struct sockaddr_in);
+    while (threadRunF) {
+        int ret = ReadFromTunfd(buffer, sizeof(buffer));
+        if (ret <= 0) {
+            continue;
+        }
+
+        // 读取到虚拟网卡的数据，通过tcp隧道，发送给客户端
+        ret = sendto(clientInfo.sockfd, buffer, ret, 0, (struct sockaddr *)&client_address, addrlen);
+        if (ret <= 0) {
+            logger_error("!!! send to client failed, ret: %d, error: %s", ret, strerror(errno));
+            std::cout << "client info " << inet_ntoa(client_address.sin_addr) << ":" << 
+                    ntohs(client_address.sin_port) << " clent fd: " << clientInfo.sockfd << "\n";
+            continue;
+        }
+    }
+}
+
+void StartUdpServer() {
+    int server_sockfd = 0;
+    struct sockaddr_in server_address;
+
+    // Create socket file descriptor
+    if ((server_sockfd = socket(AF_INET, SOCK_DGRAM, 0)) <= 0) {
+        logger_error("Failed to create socket");
+        return ;
+    }    
+
+    int on = 1;
+    setsockopt(server_sockfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+    setsockopt(server_sockfd, SOL_SOCKET, SO_REUSEPORT, &on, sizeof(on));
+
+    // Bind the socket to the port
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = INADDR_ANY;
+    server_address.sin_port = htons(PORT);
+
+    if (bind(server_sockfd, (struct sockaddr *)&server_address, sizeof(server_address)) < 0) {
+        logger_error("Failed to bind socket");
+        return ;
+    }
+
+    logger_info("Server started listening on port: %d", PORT);
+
+    ClientInfo clientInfo;
+    clientInfo.sockfd = server_sockfd;         
+
+    threadRunF = true;
+    std::thread(handle_connection, clientInfo).detach();
+    std::thread(HandleTunReceived, clientInfo).detach();
+    return ;
+}
