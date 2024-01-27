ecfaa30e54017ffc700c133bf2110c3d54082a3b
ywx1281445 <yuandongping@huawei.com>
2024-01-27 18:16:26 +0800

    VPN publicSDK整改
    Signed-off-by: ywx1281445 <yuandongping@huawei.com>

--- code/BasicFeature/Connectivity/VPN/entry/src/main/ets/entryability/EntryAbility.ts
+++ code/BasicFeature/Connectivity/VPN/entry/src/main/ets/entryability/EntryAbility.ts
@@ -21,6 +21,7 @@ import window from '@ohos.window';
 
 export default class EntryAbility extends UIAbility {
   onCreate(want: Want, launchParam: AbilityConstant.LaunchParam) {
+    AppStorage.setOrCreate('context', this.context);
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onCreate');
   }
 
