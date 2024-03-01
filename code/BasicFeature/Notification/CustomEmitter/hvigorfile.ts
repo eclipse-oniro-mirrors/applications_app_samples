be8947ea4c2e1247daf8f777e709846b6f60bcfc
yeyedeshen <yangxin181@huawei.com>
2024-03-01 14:28:29 +0800

    CustomCommonEventRely、CustomEmitter、DLPManager、FAModelAbilityDevelop、StageModelAbilityDevelop编译报错修复
    Signed-off-by: yeyedeshen <yangxin181@huawei.com>

--- code/BasicFeature/Notification/CustomEmitter/hvigorfile.ts
+++ code/BasicFeature/Notification/CustomEmitter/hvigorfile.ts
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,9 +13,5 @@
  * limitations under the License.
  */
 
-{
-  "hvigorVersion": "4.0.2",
-  "dependencies": {
-    "@ohos/hvigor-ohos-plugin": "4.0.2"
-  }
-}
+// Script for compiling build behavior. It is built in the build plug-in and cannot be modified currently.
+export { appTasks } from '@ohos/hvigor-ohos-plugin';
