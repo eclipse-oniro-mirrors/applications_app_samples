f0d39d4a4cb80ed68f087f28009ffaa1337fc4e6
产品经理的姓名 <412338663@qq.com>
2024-04-01 03:32:14 +0000

    update code/BasicFeature/Native/NdkQoS/build-profile.json5.
    Signed-off-by: 产品经理的姓名 <412338663@qq.com>
    Signed-off-by: Leibobo <412338663@qq.com>

--- code/BasicFeature/Native/NdkQoS/entry/src/main/cpp/main.cpp
+++ code/BasicFeature/Native/NdkQoS/entry/src/main/cpp/main.cpp
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include "napi/native_api.h"
 #include "qos/qos.h"
 #include <thread>
@@ -9,11 +24,13 @@ constexpr int DEPTH = 34;
 constexpr int TASKS = 10;
 constexpr int ONE = 1;
 constexpr int TWO = 2;
+constexpr int FIVE = 5;
 
-static bool AddedLoad = false;
-static double durationMillSecondTime;
+static bool g_addLoad = false;
+static double g_durationTime = 0;
 
-double fib(double n) {
+double DoFib(double n)
+{
     if (n == ONE) {
         return ONE;
     }
@@ -22,13 +39,13 @@ double fib(double n) {
         return TWO;
     }
 
-    return fib(n - ONE) + fib(n -TWO);
+    return DoFib(n - ONE) + DoFib(n - TWO);
 }
 
-void setQoS(QoS_Level level) {
+void SetQoS(QoS_Level level)
+{
     int ret = OH_QoS_SetThreadQoS(level); // 设置当前线程的QoS等级为level
-
-    if (!ret) { // ret等于0说明设置成功
+    if (!ret) {                           // ret等于0说明设置成功
         OH_LOG_Print(LOG_APP, LOG_INFO, LOG_PRINT_DOMAIN, "QoS", "set qos level success.");
         //  查询当前的qos等级
         QoS_Level querryLevel = QoS_Level::QOS_DEFAULT;
@@ -40,17 +57,17 @@ void setQoS(QoS_Level level) {
             OH_LOG_Print(LOG_APP, LOG_INFO, LOG_PRINT_DOMAIN, "QoS", "get qos level failed.");
             return;
         }
-
     } else { // ret不等于0说明设置失败
         OH_LOG_Print(LOG_APP, LOG_INFO, LOG_PRINT_DOMAIN, "QoS", "get level qos failed!");
         return;
     }
 
     auto startTime = std::chrono::system_clock::now();
-    double res = fib(DEPTH); // do calculation task
+    double res = DoFib(DEPTH); // do calculation task
     auto endTime = std::chrono::system_clock::now();
-    durationMillSecondTime = std::chrono::duration<double, std::milli>(endTime - startTime).count(); 
+    g_durationTime = std::chrono::duration<double, std::milli>(endTime - startTime).count();
     OH_LOG_Print(LOG_APP, LOG_INFO, LOG_PRINT_DOMAIN, "QoS", "calculate res is: %{public}f", res);
+    g_addLoad = false;
 
     // reset
     ret = OH_QoS_ResetThreadQoS();
@@ -74,9 +91,10 @@ void setQoS(QoS_Level level) {
 }
 
 
-void addLoads(int n) {
-    while (true) {
-        n = (n + 1) % TASKS;
+void AddLoads(int n)
+{
+    while (g_addLoad) {
+        n = (n + ONE) % TASKS;
         n *= n;
         printf("%d\n", n);
     }
@@ -84,46 +102,47 @@ void addLoads(int n) {
 
 static napi_value highQoSCalculate(napi_env env, napi_callback_info info)
 {
-    durationMillSecondTime = 0;
-    
-    if (!AddedLoad) {
+    g_durationTime = 0;
+
+    if (!g_addLoad) {
         std::vector<std::thread> loadThreads;
         for (int i = 0; i < TASKS; i++) {
-            loadThreads.emplace_back(std::thread(addLoads, 5));
+            loadThreads.emplace_back(std::thread(AddLoads, FIVE));
             loadThreads[i].detach();
         }
 
-        AddedLoad = true;
+        g_addLoad = true;
     }
 
     QoS_Level level = QoS_Level::QOS_USER_INTERACTIVE;
-    std::thread task(setQoS, level);
+    std::thread task(SetQoS, level);
     task.join();
 
     napi_value res;
-    napi_create_double(env, durationMillSecondTime, &res);
+    napi_create_double(env, g_durationTime, &res);
     return res;
 }
 
-static napi_value lowQoSCalculate(napi_env env, napi_callback_info info) {
-    durationMillSecondTime = 0;
-    
-    if (!AddedLoad) {
+static napi_value lowQoSCalculate(napi_env env, napi_callback_info info)
+{
+    g_durationTime = 0;
+
+    if (!g_addLoad) {
         std::vector<std::thread> loadThreads;
-        for (int i = 0; i < 20; i++) {
-            loadThreads.emplace_back(std::thread(addLoads, 5));
+        for (int i = 0; i < TASKS; i++) {
+            loadThreads.emplace_back(std::thread(AddLoads, FIVE));
             loadThreads[i].detach();
         }
 
-        AddedLoad = true;
+        g_addLoad = true;
     }
 
     QoS_Level level = QoS_Level::QOS_BACKGROUND;
-    std::thread task(setQoS, level);
+    std::thread task(SetQoS, level);
     task.join();
 
     napi_value res;
-    napi_create_double(env, durationMillSecondTime, &res);
+    napi_create_double(env, g_durationTime, &res);
     return res;
 }
 
@@ -132,8 +151,7 @@ static napi_value Init(napi_env env, napi_value exports)
 {
     napi_property_descriptor desc[] = {
         {"useHighQos", nullptr, highQoSCalculate, nullptr, nullptr, nullptr, napi_default, nullptr},
-        {"useLowQos", nullptr, lowQoSCalculate, nullptr, nullptr, nullptr, napi_default, nullptr}
-    };
+        {"useLowQos", nullptr, lowQoSCalculate, nullptr, nullptr, nullptr, napi_default, nullptr}};
     napi_define_properties(env, exports, sizeof(desc) / sizeof(desc[0]), desc);
     return exports;
 }
@@ -145,11 +163,8 @@ static napi_module demoModule = {
     .nm_filename = nullptr,
     .nm_register_func = Init,
     .nm_modname = "entry",
-    .nm_priv = ((void*)0),
-    .reserved = { 0 },
+    .nm_priv = ((void *)0),
+    .reserved = {0},
 };
 
-extern "C" __attribute__((constructor)) void RegisterEntryModule(void)
-{
-    napi_module_register(&demoModule);
-}
+extern "C" __attribute__((constructor)) void RegisterEntryModule(void) { napi_module_register(&demoModule); }
