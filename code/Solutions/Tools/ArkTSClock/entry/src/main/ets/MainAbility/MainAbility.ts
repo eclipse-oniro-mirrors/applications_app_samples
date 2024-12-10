e33d3babb7bec60fa7e508ac8c494ec978242258
zhanghong <zhanghong121@huawei.com>
2024-12-10 20:18:09 +0800

    cherry pick 2caacc7 from https://gitee.com/hwrtzhanghong/applications_app_samples/pulls/5013 解决时钟appfreeze
    Signed-off-by: zhanghong <zhanghong121@huawei.com>

--- code/Solutions/Tools/ArkTSClock/entry/src/main/ets/MainAbility/MainAbility.ts
+++ code/Solutions/Tools/ArkTSClock/entry/src/main/ets/MainAbility/MainAbility.ts
@@ -44,5 +44,6 @@ export default class MainAbility extends UIAbility {
   onBackground() {
     // Ability has back to background
     Logger.info('MainAbility', 'onBackground')
+    this.context.terminateSelf();
   }
 };
