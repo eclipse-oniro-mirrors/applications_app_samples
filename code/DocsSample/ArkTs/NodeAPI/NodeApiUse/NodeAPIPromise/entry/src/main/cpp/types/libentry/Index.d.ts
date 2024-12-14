881bdae2e8307c06d956afd2c019c74c53461d1d
wangweiyuan <wangweiyuan2@huawei.com>
2024-12-14 11:39:52 +0800

    ArkTSDocsSample_NodeAPIPromise
    Signed-off-by: wangweiyuan <wangweiyuan2@huawei.com>

---
+++ code/DocsSample/ArkTs/NodeAPI/NodeApiUse/NodeAPIPromise/entry/src/main/cpp/types/libentry/Index.d.ts
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export const isPromise: <T>(value: T) => boolean; // napi_is_promise
+
+export const createPromise: () => boolean | void; // napi_resolve_deferred & napi_reject_deferred
+
+export const resolveRejectDeferred: (resolve: string, reject: string, status: boolean) => Promise<string> | void;
