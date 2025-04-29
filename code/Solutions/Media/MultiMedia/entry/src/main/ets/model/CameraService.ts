8663d4f05b21808b42f8e5743877b721ede97c0e
yuandongping <yuandongping@huawei.com>
2025-04-29 17:33:48 +0800

    修复录像问题
    Signed-off-by: yuandongping <yuandongping@huawei.com>

--- code/Solutions/Media/MultiMedia/entry/src/main/ets/model/CameraService.ts
+++ code/Solutions/Media/MultiMedia/entry/src/main/ets/model/CameraService.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2022-2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,14 +13,15 @@
  * limitations under the License.
  */
 
-import camera from '@ohos.multimedia.camera'
-import deviceInfo from '@ohos.deviceInfo'
-import fileio from '@ohos.file.fs'
-import image from '@ohos.multimedia.image'
-import media from '@ohos.multimedia.media'
-import userFileManager from '@ohos.filemanagement.userFileManager'
-import Logger from '../model/Logger'
-import MediaUtils, { FileType } from '../model/MediaUtils'
+import camera from '@ohos.multimedia.camera';
+import deviceInfo from '@ohos.deviceInfo';
+import fileio from '@ohos.file.fs';
+import image from '@ohos.multimedia.image';
+import media from '@ohos.multimedia.media';
+import userFileManager from '@ohos.filemanagement.userFileManager';
+import Logger from '../model/Logger';
+import MediaUtils, { FileType } from '../model/MediaUtils';
+import { Size } from '@kit.ArkUI';
 
 const CameraMode = {
   MODE_PHOTO: 0, // 拍照模式
@@ -33,25 +34,26 @@ const CameraSize = {
 }
 
 export default class CameraService {
-  private tag: string = 'CameraService'
-  private context: any = undefined
-  private mediaUtil: MediaUtils = undefined
-  private cameraManager: camera.CameraManager = undefined
-  private cameras: Array<camera.CameraDevice> = undefined
-  private cameraId: string = ''
-  private cameraInput: camera.CameraInput = undefined
-  private previewOutput: camera.PreviewOutput = undefined
-  private photoOutPut: camera.PhotoOutput = undefined
-  private captureSession: camera.CaptureSession = undefined
-  private mReceiver: image.ImageReceiver = undefined
-  private photoUri: string = ''
-  private fileAsset: userFileManager.FileAsset = undefined
-  private fd: number = -1
-  private curMode = CameraMode.MODE_PHOTO
-  private videoRecorder: media.VideoRecorder = undefined
-  private videoOutput: camera.VideoOutput = undefined
-  private handleTakePicture: (photoUri: string) => void = undefined
-  private cameraOutputCapability: camera.CameraOutputCapability = undefined
+  private tag: string = 'CameraService';
+  private context: any = undefined;
+  private mediaUtil: MediaUtils = undefined;
+  private cameraManager: camera.CameraManager = undefined;
+  private cameras: Array<camera.CameraDevice> = undefined;
+  private cameraId: string = '';
+  private cameraInput: camera.CameraInput = undefined;
+  private previewOutput: camera.PreviewOutput = undefined;
+  private photoOutPut: camera.PhotoOutput = undefined;
+  private captureSession: camera.CaptureSession = undefined;
+  private mReceiver: image.ImageReceiver = undefined;
+  private photoUri: string = '';
+  private fileAsset: userFileManager.FileAsset = undefined;
+  private fd: number = -1;
+  private curMode = CameraMode.MODE_PHOTO;
+  private videoRecorder: media.VideoRecorder = undefined;
+  private videoOutput: camera.VideoOutput = undefined;
+  private handleTakePicture: (photoUri: string) => void = undefined;
+  private cameraOutputCapability: camera.CameraOutputCapability = undefined;
+  private previewSize: Size = undefined;
   private videoConfig: any = {
     audioSourceType: 1,
     videoSourceType: 0,
@@ -75,7 +77,7 @@ export default class CameraService {
     },
     maxSize: 10000,
     maxDuration: 10000
-  }
+  };
 
   constructor(context: any) {
     this.context = context
@@ -125,44 +127,45 @@ export default class CameraService {
   }
 
   async initCamera(surfaceId: string) {
-    Logger.info(this.tag, 'initCamera')
-    await this.releaseCamera()
-    Logger.info(this.tag, `deviceInfo.deviceType = ${deviceInfo.deviceType}`)
+    Logger.info(this.tag, 'initCamera');
+    await this.releaseCamera();
+    Logger.info(this.tag, `deviceInfo.deviceType = ${deviceInfo.deviceType}`);
     if (deviceInfo.deviceType === 'default') {
-      this.videoConfig.videoSourceType = 1
+      this.videoConfig.videoSourceType = 1;
     } else {
-      this.videoConfig.videoSourceType = 0
+      this.videoConfig.videoSourceType = 0;
     }
-    this.cameraManager = camera.getCameraManager(this.context)
-    Logger.info(this.tag, 'getCameraManager')
-    this.cameras = this.cameraManager.getSupportedCameras()
-    Logger.info(this.tag, `get cameras ${this.cameras.length}`)
+    this.cameraManager = camera.getCameraManager(this.context);
+    Logger.info(this.tag, 'getCameraManager');
+    this.cameras = this.cameraManager.getSupportedCameras();
+    Logger.info(this.tag, `get cameras ${this.cameras.length}`);
     if (this.cameras.length === 0) {
-      Logger.info(this.tag, 'cannot get cameras')
-      return
+      Logger.info(this.tag, 'cannot get cameras');
+      return;
     }
 
-    let cameraDevice = this.cameras[0]
-    this.cameraInput = this.cameraManager.createCameraInput(cameraDevice)
-    this.cameraInput.open()
-    Logger.info(this.tag, 'createCameraInput')
-    this.cameraOutputCapability = this.cameraManager.getSupportedOutputCapability(cameraDevice)
-    let previewProfile = this.cameraOutputCapability.previewProfiles[0]
-    this.previewOutput = this.cameraManager.createPreviewOutput(previewProfile, surfaceId)
-    Logger.info(this.tag, 'createPreviewOutput')
-    let mSurfaceId = await this.mReceiver.getReceivingSurfaceId()
-    let profile = this.cameraOutputCapability.photoProfiles[0]
-    this.photoOutPut = this.cameraManager.createPhotoOutput(profile, mSurfaceId)
-    this.captureSession = this.cameraManager.createCaptureSession()
-    Logger.info(this.tag, 'createCaptureSession')
-    this.captureSession.beginConfig()
-    Logger.info(this.tag, 'beginConfig')
-    this.captureSession.addInput(this.cameraInput)
-    this.captureSession.addOutput(this.previewOutput)
-    this.captureSession.addOutput(this.photoOutPut)
-    await this.captureSession.commitConfig()
-    await this.captureSession.start()
-    Logger.info(this.tag, 'captureSession start')
+    let cameraDevice = this.cameras[0];
+    this.cameraInput = this.cameraManager.createCameraInput(cameraDevice);
+    this.cameraInput.open();
+    Logger.info(this.tag, 'createCameraInput');
+    this.cameraOutputCapability = this.cameraManager.getSupportedOutputCapability(cameraDevice, camera.SceneMode.NORMAL_VIDEO);
+    let previewProfile = this.cameraOutputCapability.previewProfiles[0];
+    this.previewSize = previewProfile.size;
+    this.previewOutput = this.cameraManager.createPreviewOutput(previewProfile, surfaceId);
+    Logger.info(this.tag, 'createPreviewOutput');
+    let mSurfaceId = await this.mReceiver.getReceivingSurfaceId();
+    let profile = this.cameraOutputCapability.photoProfiles[0];
+    this.photoOutPut = this.cameraManager.createPhotoOutput(profile, mSurfaceId);
+    this.captureSession = this.cameraManager.createCaptureSession();
+    Logger.info(this.tag, 'createCaptureSession');
+    this.captureSession.beginConfig();
+    Logger.info(this.tag, 'beginConfig');
+    this.captureSession.addInput(this.cameraInput);
+    this.captureSession.addOutput(this.previewOutput);
+    this.captureSession.addOutput(this.photoOutPut);
+    await this.captureSession.commitConfig();
+    await this.captureSession.start();
+    Logger.info(this.tag, 'captureSession start');
   }
 
   setTakePictureCallback(callback) {
@@ -189,64 +192,90 @@ export default class CameraService {
   }
 
   async startVideo() {
-    Logger.info(this.tag, 'startVideo begin')
-    await this.captureSession.stop()
-    this.captureSession.beginConfig()
+    Logger.info(this.tag, 'startVideo begin');
+    await this.captureSession.stop();
+    this.captureSession.beginConfig();
     if (this.curMode === CameraMode.MODE_PHOTO) {
-      this.curMode = CameraMode.MODE_VIDEO
+      this.curMode = CameraMode.MODE_VIDEO;
       if (this.photoOutPut) {
-        this.captureSession.removeOutput(this.photoOutPut)
-        this.photoOutPut.release()
+        try {
+          this.captureSession.removeOutput(this.photoOutPut);
+          this.photoOutPut.release();
+        } catch (err) {
+          Logger.error(this.tag, 'remove photoOutPut error: ' + JSON.stringify(err));
+        }
       }
     } else {
       if (this.videoOutput) {
-        this.captureSession.removeOutput(this.videoOutput)
+        try {
+          this.captureSession.removeOutput(this.videoOutput);
+        } catch (err) {
+          Logger.error(this.tag, 'remove videoOutput error: ' + JSON.stringify(err));
+        }
       }
     }
     if (this.videoOutput) {
-      this.captureSession.removeOutput(this.videoOutput)
-      await this.videoOutput.release()
+      try {
+        this.captureSession.removeOutput(this.videoOutput);
+        await this.videoOutput.release();
+      } catch (err) {
+        Logger.error(this.tag, 'remove videoOutput error: ' + JSON.stringify(err));
+      }
     }
-    this.fileAsset = await this.mediaUtil.createAndGetUri(FileType.VIDEO)
-    this.fd = await this.mediaUtil.getFdPath(this.fileAsset)
-    this.videoRecorder = await media.createVideoRecorder()
-    this.videoConfig.url = `fd://${this.fd}`
-    await this.videoRecorder.prepare(this.videoConfig)
-    let videoId = await this.videoRecorder.getInputSurface()
+    this.fileAsset = await this.mediaUtil.createAndGetUri(FileType.VIDEO);
+    this.fd = await this.mediaUtil.getFdPath(this.fileAsset);
+    this.videoRecorder = await media.createVideoRecorder();
+    this.videoConfig.url = `fd://${this.fd}`;
+    this.videoConfig.profile.videoFrameWidth = this.previewSize.width;
+    this.videoConfig.profile.videoFrameHeight = this.previewSize.height;
+    await this.videoRecorder.prepare(this.videoConfig);
+    let videoId = await this.videoRecorder.getInputSurface();
     let profile = this.cameraOutputCapability.videoProfiles[0];
-    this.videoOutput = this.cameraManager.createVideoOutput(profile, videoId)
-    this.captureSession.addOutput(this.videoOutput)
-    await this.captureSession.commitConfig()
-    await this.captureSession.start()
-    await this.videoOutput.start()
-    await this.videoRecorder.start()
-    Logger.info(this.tag, 'startVideo end')
+    this.videoOutput = this.cameraManager.createVideoOutput(profile, videoId);
+    this.captureSession.addOutput(this.videoOutput);
+    try {
+      await this.captureSession.commitConfig();
+      await this.captureSession.start();
+      await this.videoOutput.start();
+      await this.videoRecorder.start();
+    } catch (err) {
+      Logger.error(this.tag, 'record start error: ' + JSON.stringify(err));
+    }
+    Logger.info(this.tag, 'startVideo end');
   }
 
   async stopVideo() {
-    Logger.info(this.tag, 'stopVideo called')
-    await this.videoRecorder.stop()
-    await this.videoOutput.stop()
-    await this.videoRecorder.release()
-    await this.fileAsset.close(this.fd)
+    Logger.info(this.tag, 'stopVideo called');
+    try {
+      await this.videoRecorder.stop();
+      await this.videoOutput.stop();
+      await this.videoRecorder.release();
+      await this.fileAsset.close(this.fd);
+    } catch (err) {
+      Logger.error(this.tag, 'stopVideo error: ' + JSON.stringify(err));
+    }
   }
 
   async releaseCamera() {
-    Logger.info(this.tag, 'releaseCamera')
-    if (this.cameraInput) {
-      await this.cameraInput.close()
-    }
-    if (this.previewOutput) {
-      await this.previewOutput.release()
-    }
-    if (this.photoOutPut) {
-      await this.photoOutPut.release()
-    }
-    if (this.videoOutput) {
-      await this.videoOutput.release()
-    }
-    if (this.captureSession) {
-      await this.captureSession.release()
+    Logger.info(this.tag, 'releaseCamera');
+    try {
+      if (this.cameraInput) {
+        await this.cameraInput.close();
+      }
+      if (this.previewOutput) {
+        await this.previewOutput.release();
+      }
+      if (this.photoOutPut) {
+        await this.photoOutPut.release();
+      }
+      if (this.videoOutput) {
+        await this.videoOutput.release();
+      }
+      if (this.captureSession) {
+        await this.captureSession.release();
+      }
+    } catch (err) {
+      Logger.error(this.tag, 'releaseCamera error: ' + JSON.stringify(err));
     }
   }
 }
