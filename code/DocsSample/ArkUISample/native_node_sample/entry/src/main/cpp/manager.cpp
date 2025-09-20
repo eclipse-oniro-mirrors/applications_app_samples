1eed9f4c83de935950f0911d6f4171286b124f2e
liuyongkai2 <liuyongkai2@huawei-partners.com>
2025-09-20 14:40:57 +0800

    add_native_node_sample
    Signed-off-by: liuyongkai2 <liuyongkai2@huawei-partners.com>

---
+++ code/DocsSample/ArkUISample/native_node_sample/entry/src/main/cpp/manager.cpp
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "manager.h"
+#include "TextMaker.h"
+#include "baseUtils.h"
+#include "napi/native_api.h"
+#include <arkui/native_interface.h>
+#include <arkui/native_node.h>
+#include <arkui/native_node_napi.h>
+#include <arkui/native_type.h>
+#include <hilog/log.h>
+
+namespace ConstIde {
+    const uint32_t NUMBER_0 = 0;
+    const uint32_t NUMBER_1 = 1;
+    const uint32_t NUMBER_2 = 2;
+    const uint32_t MARGIN_NUMBER_30 = 30;
+}
+
+Manager Manager::manager_;
+ArkUI_NativeNodeAPI_1 *Manager::nodeAPI_ = reinterpret_cast<ArkUI_NativeNodeAPI_1*>(
+    OH_ArkUI_QueryModuleInterfaceByName(ARKUI_NATIVE_NODE, "ArkUI_NativeNodeAPI_1"));
+
+napi_value Manager::CreateNativeTextNode(napi_env env, napi_callback_info info)
+{
+    OH_LOG_Print(LOG_APP, LOG_INFO, LOG_PRINT_DOMAIN, "Manager", "CreateNativeNode BEGIN");
+    if ((env == nullptr) || (info == nullptr)) {
+        OH_LOG_Print(LOG_APP, LOG_INFO, LOG_PRINT_DOMAIN, "Manager", "CreateNativeNode env or info is null");
+        return nullptr;
+    }
+    size_t argCnt = ConstIde::NUMBER_1;
+    napi_value args[ConstIde::NUMBER_1] = {nullptr};
+    if (napi_get_cb_info(env, info, &argCnt, args, nullptr, nullptr) != napi_ok) {
+        OH_LOG_Print(LOG_APP, LOG_INFO, LOG_PRINT_DOMAIN, "Manager", "CreateNativeNode napi_get_cb_info failed");
+    }
+
+    ArkUI_NodeContentHandle nodeContentHandle = nullptr;
+
+    OH_ArkUI_GetNodeContentFromNapiValue(env, args[ConstIde::NUMBER_0], &nodeContentHandle);
+
+    OH_LOG_Print(LOG_APP, LOG_INFO, LOG_PRINT_DOMAIN, "Manager", "OH_ArkUI_GetBasicNodeAPI after");
+    if (nodeAPI_ != nullptr) {
+        if (nodeAPI_->createNode != nullptr && nodeAPI_->addChild != nullptr) {
+            ArkUI_NodeHandle testNode = TextMaker::createTextPage();
+            OH_ArkUI_NodeContent_AddNode(nodeContentHandle, testNode);
+        }
+    }
+    return nullptr;
+}
