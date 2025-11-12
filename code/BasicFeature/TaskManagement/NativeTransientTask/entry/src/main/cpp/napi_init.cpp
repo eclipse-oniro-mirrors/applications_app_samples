0edb707bc52bb99b954c391f2183574aa694db83
zhushuanghong <zhushuanghong5@huawei.com>
2025-11-12 18:37:42 +0800

    native短时任务sample同源
    Signed-off-by: zhushuanghong <zhushuanghong5@huawei.com>

--- code/BasicFeature/TaskManagement/NativeTransientTask/entry/src/main/cpp/napi_init.cpp
+++ code/BasicFeature/TaskManagement/NativeTransientTask/entry/src/main/cpp/napi_init.cpp
@@ -18,8 +18,9 @@
 #include "transient_task/transient_task_api.h"
 
 TransientTask_DelaySuspendInfo delaySuspendInfo;
+const int32_t TransientTask_TIMER = 3;
 
-static void callback(void)
+static void Callback(void)
 {
    // 短时任务即将结束，业务在这里取消短时任务
    OH_BackgroundTaskManager_CancelSuspendDelay(delaySuspendInfo.requestId);
@@ -28,37 +29,37 @@ static void callback(void)
 // 申请短时任务
 static napi_value RequestSuspendDelay(napi_env env, napi_callback_info info)
 {
-      napi_value result;
-      int32_t res = OH_BackgroundTaskManager_RequestSuspendDelay("test", callback, &delaySuspendInfo);
-      if (res == 0) {
-         napi_create_int32(env, delaySuspendInfo.requestId, &result);
-      } else {
-         napi_create_int32(env, -1, &result);
-      }
-      return result;
+   napi_value result;
+   int32_t res = OH_BackgroundTaskManager_RequestSuspendDelay("test", Callback, &delaySuspendInfo);
+   if (res == 0) {
+      napi_create_int32(env, delaySuspendInfo.requestId, &result);
+   } else {
+      napi_create_int32(env, -1, &result);
+   }
+   return result;
 }
 
 // 获取剩余时间
 static napi_value GetRemainingDelayTime(napi_env env, napi_callback_info info)
 {
-      napi_value result;
-      int32_t delayTime = 0;
-      int32_t res = OH_BackgroundTaskManager_GetRemainingDelayTime(delaySuspendInfo.requestId, &delayTime);
-      if (res == 0) {
-         napi_create_int32(env, delayTime, &result);
-      } else {
-         napi_create_int32(env, -1, &result);
-      }
-      return result;
+   napi_value result;
+   int32_t delayTime = 0;
+   int32_t res = OH_BackgroundTaskManager_GetRemainingDelayTime(delaySuspendInfo.requestId, &delayTime);
+   if (res == 0) {
+      napi_create_int32(env, delayTime, &result);
+   } else {
+      napi_create_int32(env, -1, &result);
+   }
+   return result;
 }
 
 // 取消短时任务
 static napi_value CancelSuspendDelay(napi_env env, napi_callback_info info)
 {
-      napi_value result;
-      int32_t res = OH_BackgroundTaskManager_CancelSuspendDelay(delaySuspendInfo.requestId);
-      napi_create_int32(env, res, &result);
-      return result;
+   napi_value result;
+   int32_t res = OH_BackgroundTaskManager_CancelSuspendDelay(delaySuspendInfo.requestId);
+   napi_create_int32(env, res, &result);
+   return result;
 }
 
 // 获取所有短时任务信息
@@ -79,9 +80,9 @@ static napi_value GetTransientTaskInfo(napi_env env, napi_callback_info info)
       napi_create_array(env, &info);
       uint32_t count = 0;
       // 格式化所有已申请的短时任务信息
-      for (int index = 0; index < 3; index++) {
+      for (int index = 0; index < TransientTask_TIMER; index++) {
          if (transientTaskInfo.transientTasks[index].requestId == 0) {
-             continue;
+            continue;
          }
          
          napi_value napiWork = nullptr;
@@ -115,31 +116,31 @@ static napi_value GetTransientTaskInfo(napi_env env, napi_callback_info info)
 EXTERN_C_START
 static napi_value Init(napi_env env, napi_value exports)
 {
-    napi_property_descriptor desc[] = {
-        {"RequestSuspendDelay", nullptr, RequestSuspendDelay, nullptr, nullptr, nullptr, napi_default, nullptr},
-        {"GetRemainingDelayTime", nullptr, GetRemainingDelayTime, nullptr, nullptr, nullptr, napi_default, nullptr},
-        {"CancelSuspendDelay", nullptr, CancelSuspendDelay, nullptr, nullptr, nullptr, napi_default, nullptr},
-        {"GetTransientTaskInfo", nullptr, GetTransientTaskInfo, nullptr, nullptr, nullptr, napi_default, nullptr },
-    };
-    napi_define_properties(env, exports, sizeof(desc) / sizeof(desc[0]), desc);
-    return exports;
+   napi_property_descriptor desc[] = {
+      {"RequestSuspendDelay", nullptr, RequestSuspendDelay, nullptr, nullptr, nullptr, napi_default, nullptr},
+      {"GetRemainingDelayTime", nullptr, GetRemainingDelayTime, nullptr, nullptr, nullptr, napi_default, nullptr},
+      {"CancelSuspendDelay", nullptr, CancelSuspendDelay, nullptr, nullptr, nullptr, napi_default, nullptr},
+      {"GetTransientTaskInfo", nullptr, GetTransientTaskInfo, nullptr, nullptr, nullptr, napi_default, nullptr },
+   };
+   napi_define_properties(env, exports, sizeof(desc) / sizeof(desc[0]), desc);
+   return exports;
 }
 EXTERN_C_END
 // [End registration_function]
 
 // [Start registration_module]
 static napi_module demoModule = {
-    .nm_version = 1,
-    .nm_flags = 0,
-    .nm_filename = nullptr,
-    .nm_register_func = Init,
-    .nm_modname = "entry",
-    .nm_priv = ((void*)0),
-    .reserved = { 0 },
+   .nm_version = 1,
+   .nm_flags = 0,
+   .nm_filename = nullptr,
+   .nm_register_func = Init,
+   .nm_modname = "entry",
+   .nm_priv = ((void*)0),
+   .reserved = { 0 },
 };
 
 extern "C" __attribute__((constructor)) void RegisterEntryModule(void)
 {
-    napi_module_register(&demoModule);
+   napi_module_register(&demoModule);
 }
 // [End registration_module]
