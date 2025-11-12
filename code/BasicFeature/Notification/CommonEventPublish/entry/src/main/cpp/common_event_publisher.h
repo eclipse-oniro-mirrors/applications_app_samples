65ffd47052585732ce37ac4660edf340e3a5dabd
tianlongdevcode <tianlong18@h-partners.com>
2025-11-12 14:08:09 +0800

    Add common event subscription samples
    Signed-off-by: tianlongdevcode <tianlong18@h-partners.com>

---
+++ code/BasicFeature/Notification/CommonEventPublish/entry/src/main/cpp/common_event_publisher.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef COMMON_EVENT_PUBLISHER_H
+#define COMMON_EVENT_PUBLISHER_H
+
+// [Start event_publisher_import]
+#include <cstdint>
+#include <cstdio>
+#include <cwchar>
+#include <cstring>
+#include "hilog/log.h"
+#include "BasicServicesKit/oh_commonevent.h"
+// [End event_publisher_import]
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void Publish(const char* event);
+
+void PublishWithInfo(const char* event, CommonEvent_PublishInfo* info);
+
+void SetPublishInfo(const char* bundleName, const char* permissions[], int32_t num, const int32_t code,
+                    const char* data);
+CommonEvent_Parameters* CreateParameters();
+
+void DestroyPublishInfo(CommonEvent_Parameters* param, CommonEvent_PublishInfo* info);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // COMMON_EVENT_PUBLISHER_H
