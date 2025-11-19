06319503f6acf66b2332711b36e4799cc0b5179b
chenbosongcode <chenbosong3@h-partners.com>
2025-11-19 16:56:34 +0800

    IssueNo: #7510：Form领域格式化修改 Description: Form领域格式化修改 Sig: SIG_Sample Feature or Bugfix: Feature Binary Source: No
    Signed-off-by: niuyongqing <tianlong18@h-partners.com>

--- code/DocsSample/ApplicationModels/StageServiceWidgetCards/entry/src/main/ets/wgtimgupdateentryformability/WgtImgUpdateEntryFormAbility.ts
+++ code/DocsSample/ApplicationModels/StageServiceWidgetCards/entry/src/main/ets/wgtimgupdateentryformability/WgtImgUpdateEntryFormAbility.ts
@@ -27,10 +27,10 @@ import { http } from '@kit.NetworkKit';
 // entry/src/main/ets/wgtimgupdateentryformability/WgtImgUpdateEntryFormAbility.ts
 const TAG: string = 'WgtImgUpdateEntryFormAbility';
 const DOMAIN_NUMBER: number = 0xFF00;
- // [StartExclude local_file_refresh]
+// [StartExclude local_file_refresh]
 const TEXT1: string = '刷新中...'
 const TEXT2: string = '刷新失败'
- // [EndExclude local_file_refresh]
+// [EndExclude local_file_refresh]
 
 export default class WgtImgUpdateEntryFormAbility extends FormExtensionAbility {
   // [StartExclude network_file_refresh]
