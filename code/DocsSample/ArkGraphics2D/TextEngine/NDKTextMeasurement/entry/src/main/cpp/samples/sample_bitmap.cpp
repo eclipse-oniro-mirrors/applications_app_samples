c3ae73539e0915aaf44adeb4364fb28cff6a4361
Lem0nC <caimengyu2@huawei.com>
2025-12-15 10:35:27 +0800

    fix sample text measure
    Signed-off-by: Lem0nC <caimengyu2@huawei.com>
    Change-Id: I555fd1f0cc3df257ddd23df2553b30d5946e5323

--- code/DocsSample/ArkGraphics2D/TextEngine/NDKTextMeasurement/entry/src/main/cpp/samples/sample_bitmap.cpp
+++ code/DocsSample/ArkGraphics2D/TextEngine/NDKTextMeasurement/entry/src/main/cpp/samples/sample_bitmap.cpp
@@ -14,9 +14,11 @@
  */
 
 #include <bits/alltypes.h>
+// [Start c_text_metrics_include]
 #include <native_drawing/drawing_font_collection.h>
 #include <native_drawing/drawing_text_typography.h>
 #include <native_drawing/drawing_text_declaration.h>
+// [End c_text_metrics_include]
 #include <native_drawing/drawing_register_font.h>
 #include <native_buffer/native_buffer.h>
 
@@ -168,25 +170,31 @@ void SampleBitMap::DrawText()
     OH_Drawing_FontCollection *fontCollection = OH_Drawing_GetFontCollectionGlobalInstance();
     OH_Drawing_TextStyle *myTextStyle = OH_Drawing_CreateTextStyle();
 
-    // 设置其他文本样式
+    // [Start c_text_metrics_create_paragraph]
+    // 创建文本样式，并设置字体大小为50
     OH_Drawing_SetTextStyleColor(myTextStyle, OH_Drawing_ColorSetArgb(0xFF, 0x00, 0x00, 0x00));
     OH_Drawing_SetTextStyleFontSize(myTextStyle, 50.0);
     // 创建一个段落样式对象，以设置排版风格
     OH_Drawing_TypographyStyle *typographyStyle = OH_Drawing_CreateTypographyStyle();
-    OH_Drawing_SetTypographyTextAlign(typographyStyle, TEXT_ALIGN_LEFT); // 设置段落样式为左对齐
+    // 设置段落样式的对齐方式为左对齐
+    OH_Drawing_SetTypographyTextAlign(typographyStyle, TEXT_ALIGN_LEFT);
     // 创建一个段落生成器
     OH_Drawing_TypographyCreate *handler = OH_Drawing_CreateTypographyHandler(typographyStyle, fontCollection);
     // 在段落生成器中设置文本样式
     OH_Drawing_TypographyHandlerPushTextStyle(handler, myTextStyle);
-    // 在段落生成器中设置文本内容
+    // 在段落生成器中添加文本内容
     const char *text = "排版测量的文字度量信息";
     OH_Drawing_TypographyHandlerAddText(handler, text);
     // 通过段落生成器生成段落
     OH_Drawing_Typography *typography = OH_Drawing_CreateTypography(handler);
+    // [End c_text_metrics_create_paragraph]
 
-    // 设置页面最大宽度
+    // 设置maxWidth为页面最大宽度
     double maxWidth = width_;
+    // [Start c_text_metrics_layout]
+    // 对段落进行塑形排版，设置排版宽度为maxWidth
     OH_Drawing_TypographyLayout(typography, maxWidth);
+    // [End c_text_metrics_layout]
     // 将文本绘制到画布上
     OH_Drawing_TypographyPaint(typography, cCanvas_, 0, 100);
     // [Start c_text_metrics_get_all_case]
