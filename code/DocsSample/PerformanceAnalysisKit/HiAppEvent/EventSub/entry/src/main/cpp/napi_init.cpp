81c3d2542d90116ff3c9b93bb95ddacca1a3a6b7
zhou_xq <zhouxiaoqin4@h-partners.com>
2026-01-05 12:49:34 +0800

    为json.h的路径添加注释
    Signed-off-by: zhou_xq <zhouxiaoqin4@h-partners.com>

--- code/DocsSample/PerformanceAnalysisKit/HiAppEvent/EventSub/entry/src/main/cpp/napi_init.cpp
+++ code/DocsSample/PerformanceAnalysisKit/HiAppEvent/EventSub/entry/src/main/cpp/napi_init.cpp
@@ -22,6 +22,7 @@
 
 // [Start EventSub_napi_Header]
 #include "napi/native_api.h"
+// 根据工程中三方库jsoncpp的位置适配引用json.h的路径
 #include "../../../build/jsoncpp-1.9.6/include/json/json.h"
 #include "hilog/log.h"
 #include "hiappevent/hiappevent.h"
