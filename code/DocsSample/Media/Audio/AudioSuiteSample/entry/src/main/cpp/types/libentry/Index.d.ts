76b1b965d55eb96b7bd6b44a6fc156ff26991297
renzhaohu <renzhaohu1@h-partners.com>
2026-05-21 22:16:51 +0800

    sample修改
    Signed-off-by: renzhaohu <renzhaohu1@h-partners.com>

--- code/DocsSample/Media/Audio/AudioSuiteSample/entry/src/main/cpp/types/libentry/Index.d.ts
+++ code/DocsSample/Media/Audio/AudioSuiteSample/entry/src/main/cpp/types/libentry/Index.d.ts
@@ -2,8 +2,10 @@ export const CreateAudioRender: (type: number) => string;
 
 export const DestroyAudioRender: (type: number) => string;
 
-export const BaseEditor: () => string;
+export const BaseEditor: (callback: (result: string) => void) => void;
 
-export const AudioSourceSeparationNapi: () => string;
+export const AudioSourceSeparationNapi: (callback: (result: string) => void) => void;
+
+export const MixingAndCascadingNapi: (callback: (result: string) => void) => void;
 
 export const EqualizerEffectNapi: () => string;
