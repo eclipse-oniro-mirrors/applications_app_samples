f8283daac8fe868d9381b6a87a358c6644d7ce0b
liuhuayi_openharmony <liuhuayi3@huawei.com>
2026-06-11 21:16:41 +0800

    fix format check
    Signed-off-by: liuhuayi_openharmony <liuhuayi3@huawei.com>

--- code/UI/NdkBuildOnMultiThread/entry/src/main/cpp/common/NativeModule.h
+++ code/UI/NdkBuildOnMultiThread/entry/src/main/cpp/common/NativeModule.h
@@ -27,40 +27,45 @@ namespace NativeModule {
 namespace {
 void MultiThreadNodeApiExample()
 {
-// [Start multi_thread_node_api_start]
-ArkUI_NativeNodeAPI_1 *multiThreadNodeAPI = nullptr;
-// 获取多线程NDK接口集合。
-OH_ArkUI_GetModuleInterface(ARKUI_MULTI_THREAD_NATIVE_NODE, ArkUI_NativeNodeAPI_1, multiThreadNodeAPI);
+    // [Start multi_thread_node_api_start]
+    ArkUI_NativeNodeAPI_1 *multiThreadNodeAPI = nullptr;
+    // 获取多线程NDK接口集合。
+    OH_ArkUI_GetModuleInterface(ARKUI_MULTI_THREAD_NATIVE_NODE, ArkUI_NativeNodeAPI_1, multiThreadNodeAPI);
 
-if (!multiThreadNodeAPI) {
-  return;
-}
-// 调用集合中支持多线程的createNode接口创建UI组件。
-auto node = multiThreadNodeAPI->createNode(ARKUI_NODE_COLUMN);
-// [StartExclude multi_thread_node_api_start]
-if (node) {
-    multiThreadNodeAPI->disposeNode(node);
-}
-// [EndExclude multi_thread_node_api_start]
-// [End multi_thread_node_api_start]
+    if (!multiThreadNodeAPI) {
+        return;
+    }
+    // 调用集合中支持多线程的createNode接口创建UI组件。
+    auto node = multiThreadNodeAPI->createNode(ARKUI_NODE_COLUMN);
+    // [StartExclude multi_thread_node_api_start]
+    if (node) {
+        multiThreadNodeAPI->disposeNode(node);
+    }
+    // [EndExclude multi_thread_node_api_start]
+    // [End multi_thread_node_api_start]
 }
 } // namespace
 // [EndExclude native_module_start]
 
 class NativeModuleInstance {
 public:
-    static NativeModuleInstance *GetInstance() {
+    static NativeModuleInstance *GetInstance()
+    {
         static NativeModuleInstance instance;
         return &instance;
     }
 
-    NativeModuleInstance() {
+    NativeModuleInstance()
+    {
         // 获取多线程NDK接口的函数指针结构体对象，用于后续操作。
         OH_ArkUI_GetModuleInterface(ARKUI_MULTI_THREAD_NATIVE_NODE, ArkUI_NativeNodeAPI_1, arkUINativeNodeApi_);
         assert(arkUINativeNodeApi_);
     }
     // 暴露给其他模块使用。
-    ArkUI_NativeNodeAPI_1 *GetNativeNodeAPI() { return arkUINativeNodeApi_; }
+    ArkUI_NativeNodeAPI_1 *GetNativeNodeAPI()
+    {
+        return arkUINativeNodeApi_;
+    }
 
 private:
     ArkUI_NativeNodeAPI_1 *arkUINativeNodeApi_ = nullptr;
