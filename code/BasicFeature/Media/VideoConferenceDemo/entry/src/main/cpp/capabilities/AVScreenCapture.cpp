9ffa1d61d49b23cd0c3eda979cffb39a7f689160
Cencus <lewis.nexus@foxmail.com>
2026-07-08 17:27:07 +0800

    fix: 移除hilog依赖及NDK库引用，修复WordsTool规范问题
    Signed-off-by: Cencus <lewis.nexus@foxmail.com>

--- code/BasicFeature/Media/VideoConferenceDemo/entry/src/main/cpp/capabilities/AVScreenCapture.cpp
+++ code/BasicFeature/Media/VideoConferenceDemo/entry/src/main/cpp/capabilities/AVScreenCapture.cpp
@@ -7,14 +7,13 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an 'AS IS' BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
 
 #include "AVScreenCapture.h"
-#include "hilog/log.h"
 #include <js_native_api.h>
 #include <multimedia/player_framework/native_avscreen_capture.h>
 #include <multimedia/player_framework/native_avscreen_capture_errors.h>
@@ -22,11 +21,6 @@
 #include <node_api_types.h>
 #include <thread>
 #include <string>
-
-#undef LOG_DOMAIN
-#undef LOG_TAG
-#define LOG_DOMAIN 0x3200
-#define LOG_TAG "MY_AVSCREENCAPTURE"
 
 bool g_isRunning = false;
 OH_AVScreenCapture *g_avScreenCapture;
@@ -54,17 +48,13 @@ void AVScreenCapture::StopScreenCaptureRecording(struct OH_AVScreenCapture *capt
     if (g_isRunning && capture != nullptr) {
         OH_AVScreenCapture_StopScreenRecording(capture);
         g_isRunning = false;
-        OH_LOG_INFO(LOG_APP, "AVScreenCapture ScreenCapture StopScreenCapture");
     }
 }
 
-/*
- * Screen recording Error callback
- */
 void AVScreenCapture::OnErrorSaveFile(OH_AVScreenCapture *capture, int32_t errorCode, void *userData)
 {
     (void)capture;
-    OH_LOG_INFO(LOG_APP, "AVScreenCapture ScreenCapture OnError errorCode is %{public}d", errorCode);
+    (void)errorCode;
     (void)userData;
 }
 
@@ -73,7 +63,6 @@ void AVScreenCapture::ReleaseAVScreenCapture(struct OH_AVScreenCapture *capture)
 {
     StopScreenCaptureRecording(capture);
     if (capture != nullptr) {
-        OH_LOG_INFO(LOG_APP, "AVScreenCapture ScreenCapture ReleaseSCInstanceWorker S");
         OH_AVScreenCapture_Release(capture);
         g_isRunning = false;
         g_avScreenCapture = nullptr;
@@ -81,9 +70,6 @@ void AVScreenCapture::ReleaseAVScreenCapture(struct OH_AVScreenCapture *capture)
 }
 // [End ReleaseAVScreenCapture]
 
-/*
- * Screen recording state change callback
- */
 void AVScreenCapture::OnStateChangeSaveFile(struct OH_AVScreenCapture *capture,
                                             OH_AVScreenCaptureStateCode stateCode, void *userData)
 {
@@ -92,42 +78,18 @@ void AVScreenCapture::OnStateChangeSaveFile(struct OH_AVScreenCapture *capture,
     switch (stateCode) {
         case OH_SCREEN_CAPTURE_STATE_STARTED: {
             CallStartCallback();
-            OH_LOG_INFO(LOG_APP, "AVScreenCapture ScreenCapture OnStateChange OH_SCREEN_CAPTURE_STATE_STARTED");
             break;
         }
         case OH_SCREEN_CAPTURE_STATE_CANCELED: {
             std::thread releaseInstanceThread(ReleaseAVScreenCapture, capture);
             releaseInstanceThread.detach();
             CallStopCallback();
-            OH_LOG_INFO(LOG_APP, "AVScreenCapture ScreenCapture OnStateChange OH_SCREEN_CAPTURE_STATE_CANCELED ");
             break;
         }
-        case OH_SCREEN_CAPTURE_STATE_STOPPED_BY_CALL:
-            OH_LOG_INFO(LOG_APP,
-                        "AVScreenCapture ScreenCapture OnStateChange OH_SCREEN_CAPTURE_STATE_STOPPED_BY_CALL");
-            break;
-        case OH_SCREEN_CAPTURE_STATE_MIC_UNAVAILABLE:
-            OH_LOG_INFO(LOG_APP,
-                        "AVScreenCapture ScreenCapture OnStateChange OH_SCREEN_CAPTURE_STATE_MIC_UNAVAILABLE");
-            break;
-        case OH_SCREEN_CAPTURE_STATE_INTERRUPTED_BY_OTHER:
-            OH_LOG_INFO(LOG_APP,
-                        "AVScreenCapture ScreenCapture OnStateChange OH_SCREEN_CAPTURE_STATE_INTERRUPTED_BY_OTHER");
-            break;
-        case OH_SCREEN_CAPTURE_STATE_MIC_MUTED_BY_USER:
-        case OH_SCREEN_CAPTURE_STATE_MIC_UNMUTED_BY_USER:
-            OH_LOG_INFO(LOG_APP, "AVScreenCapture OnStateChange MIC %{public}d", static_cast<int>(stateCode));
-            break;
-        case OH_SCREEN_CAPTURE_STATE_ENTER_PRIVATE_SCENE:
-        case OH_SCREEN_CAPTURE_STATE_EXIT_PRIVATE_SCENE:
-            OH_LOG_INFO(LOG_APP, "AVScreenCapture OnStateChange PRIVATE %{public}d", static_cast<int>(stateCode));
-            break;
         case OH_SCREEN_CAPTURE_STATE_STOPPED_BY_USER: {
             std::thread releaseInstanceThread(ReleaseAVScreenCapture, capture);
             releaseInstanceThread.detach();
             CallStopCallback();
-            OH_LOG_INFO(LOG_APP,
-                        "AVScreenCapture ScreenCapture OnStateChange OH_SCREEN_CAPTURE_STATE_STOPPED_BY_USER");
             break;
         }
         default:
@@ -136,36 +98,28 @@ void AVScreenCapture::OnStateChangeSaveFile(struct OH_AVScreenCapture *capture,
 }
 
 // [Start SetConfig]
-// Configuration parameters
 void AVScreenCapture::SetConfig(OH_AVScreenCaptureConfig &config)
 {
     OH_RecorderInfo recorderInfo;
     recorderInfo.fileFormat = OH_ContainerFormatType::CFT_MPEG_4A;
-    // Config VideoCaptureInfo
     OH_VideoCaptureInfo videoCapInfo = {
         .videoFrameWidth = 0, .videoFrameHeight = 0, .videoSource = OH_VIDEO_SOURCE_SURFACE_RGBA};
-    // Config VideoEncInfo
     OH_VideoEncInfo videoEncInfo = {
         .videoCodec = OH_VideoCodecFormat::OH_H264, .videoBitrate = 2000000, .videoFrameRate = 30};
-    // Config VideoInfo
     OH_VideoInfo videoInfo = {.videoCapInfo = videoCapInfo, .videoEncInfo = videoEncInfo};
 
-    // Config Mic Capture Info
     OH_AudioCaptureInfo micCapInfo = {.audioSampleRate = 48000, .audioChannels = 2, .audioSource = OH_MIC};
-    // Config inner Capture Info
     OH_AudioCaptureInfo innerCapInfo = {
         .audioSampleRate = 48000, .audioChannels = 2, .audioSource = OH_ALL_PLAYBACK};
-    // Config Audio Encoder Info
     OH_AudioEncInfo audioEncInfo = {
         .audioBitrate = 96000, .audioCodecformat = OH_AudioCodecFormat::OH_AAC_LC};
-    // Config Audio Info
     OH_AudioInfo audioInfo = {.micCapInfo = micCapInfo, .innerCapInfo = innerCapInfo, .audioEncInfo = audioEncInfo};
 
-    config.captureMode = OH_CAPTURE_HOME_SCREEN; // screen capture mode
-    config.dataType = OH_CAPTURE_FILE;           // data type
-    config.audioInfo = audioInfo;                // audio info
-    config.videoInfo = videoInfo;                // video info
-    config.recorderInfo = recorderInfo;          // recorder info
+    config.captureMode = OH_CAPTURE_HOME_SCREEN;
+    config.dataType = OH_CAPTURE_FILE;
+    config.audioInfo = audioInfo;
+    config.videoInfo = videoInfo;
+    config.recorderInfo = recorderInfo;
 }
 // [End SetConfig]
 
@@ -175,21 +129,8 @@ OH_AVSCREEN_CAPTURE_ErrCode AVScreenCapture::StopScreenCaptureToFile()
     OH_AVSCREEN_CAPTURE_ErrCode result = AV_SCREEN_CAPTURE_ERR_OPERATE_NOT_PERMIT;
 
     if (g_isRunning && g_avScreenCapture != nullptr) {
-        OH_LOG_INFO(LOG_APP, "AVScreenCapture ScreenCapture File Stop");
         result = OH_AVScreenCapture_StopScreenRecording(g_avScreenCapture);
-        if (result != AV_SCREEN_CAPTURE_ERR_BASE) {
-            OH_LOG_ERROR(LOG_APP,
-                         "AVScreenCapture StopScreenCapture OH_AVScreenCapture_StopScreenRecording Result: %{public}d",
-                         result);
-        } else {
-            OH_LOG_INFO(LOG_APP, "AVScreenCapture StopScreenCapture OH_AVScreenCapture_StopScreenRecording");
-        }
         result = OH_AVScreenCapture_Release(g_avScreenCapture);
-        if (result != AV_SCREEN_CAPTURE_ERR_BASE) {
-            OH_LOG_ERROR(LOG_APP, "AVScreenCapture StopScreenCapture OH_AVScreenCapture_Release: %{public}d", result);
-        } else {
-            OH_LOG_INFO(LOG_APP, "AVScreenCapture OH_AVScreenCapture_Release success");
-        }
         g_isRunning = false;
         g_avScreenCapture = nullptr;
     }
@@ -215,7 +156,6 @@ OH_AVSCREEN_CAPTURE_ErrCode AVScreenCapture::StartScreenCaptureToFile(int32_t ou
     }
     g_avScreenCapture = OH_AVScreenCapture_Create();
     if (g_avScreenCapture == nullptr) {
-        OH_LOG_ERROR(LOG_APP, "AVScreenCapture create screen capture failed");
         return AV_SCREEN_CAPTURE_ERR_INVALID_VAL;
     }
     OH_AVScreenCaptureConfig config_;
@@ -229,16 +169,15 @@ OH_AVSCREEN_CAPTURE_ErrCode AVScreenCapture::StartScreenCaptureToFile(int32_t ou
 
     OH_AVSCREEN_CAPTURE_ErrCode result = OH_AVScreenCapture_Init(g_avScreenCapture, config_);
     if (result != AV_SCREEN_CAPTURE_ERR_OK) {
-        OH_LOG_INFO(LOG_APP, "AVScreenCapture ScreenCapture OH_AVScreenCapture_Init failed %{public}d", result);
+        OH_AVScreenCapture_Release(g_avScreenCapture);
+        return result;
     }
-    OH_LOG_INFO(LOG_APP, "AVScreenCapture ScreenCapture OH_AVScreenCapture_Init succ %{public}d", result);
 
     result = OH_AVScreenCapture_StartScreenRecording(g_avScreenCapture);
     if (result != AV_SCREEN_CAPTURE_ERR_OK) {
-        OH_LOG_INFO(LOG_APP, "AVScreenCapture ScreenCapture Started failed %{public}d", result);
         OH_AVScreenCapture_Release(g_avScreenCapture);
+        return result;
     }
-    OH_LOG_INFO(LOG_APP, "AVScreenCapture ScreenCapture Started succ %{public}d", result);
     g_isRunning = true;
     return result;
 }
