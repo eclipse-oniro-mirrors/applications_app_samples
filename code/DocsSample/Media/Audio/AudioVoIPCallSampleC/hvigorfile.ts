5a3694b3d4f519e4a75a661186523eb276f11c85
ZhengYong21 <zhengyong21@huawei.com>
2026-07-16 15:48:34 +0800

    update: 更新AudioVoIPCallSampleC配置
    Co-Authored-By: Agent
    Signed-off-by: ZhengYong21 <zhengyong21@huawei.com>

--- code/DocsSample/Media/Audio/AudioVoIPCallSampleC/hvigorfile.ts
+++ code/DocsSample/Media/Audio/AudioVoIPCallSampleC/hvigorfile.ts
@@ -16,5 +16,5 @@ import { appTasks } from '@ohos/hvigor-ohos-plugin';
 
 export default {
   system: appTasks, /* Built-in plugin of Hvigor. It cannot be modified. */
-  plugins: []       /* Custom plugin to extend the functionality of Hvigor. */
-}
+  plugins: []
+};
