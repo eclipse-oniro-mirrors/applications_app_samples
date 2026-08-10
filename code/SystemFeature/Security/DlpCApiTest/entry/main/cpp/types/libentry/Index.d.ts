cdc1164b07a4c3bf3239dc24eb7c69b8c4698a8b
gcw_RYdx6Zzw <zhongjie35@huawei.com>
2026-08-10 20:19:17 +0800

    delete: 删除文件夹 code/SystemFeature/Security/DlpCApiTest/entry/main
    Signed-off-by: gcw_RYdx6Zzw <zhongjie35@huawei.com>

--- code/SystemFeature/Security/DlpCApiTest/entry/main/cpp/types/libentry/Index.d.ts
+++
@@ -1,6 +0,0 @@
-export const GetDlpPermissionInfo: () => number;
-export const GetOriginalFileName: () => number;
-export const IsInSandbox: () => boolean;
-export const SetSandboxAppConfig: () => number;
-export const GetSandboxAppConfig: () => number;
-export const CleanSandboxAppConfig: () => number;
