261a09f9a2513cffd81d23c059b9e2bf2a09f911
gcw_RYdx6Zzw <zhongjie35@huawei.com>
2026-08-10 20:00:59 +0800

    new: 新建文件 Index.d.ts
    Signed-off-by: gcw_RYdx6Zzw <zhongjie35@huawei.com>

---
+++ code/SystemFeature/Security/DlpCApiTest/entry/main/cpp/types/libentry/Index.d.ts
@@ -0,0 +1,6 @@
+export const GetDlpPermissionInfo: () => number;
+export const GetOriginalFileName: () => number;
+export const IsInSandbox: () => boolean;
+export const SetSandboxAppConfig: () => number;
+export const GetSandboxAppConfig: () => number;
+export const CleanSandboxAppConfig: () => number;
