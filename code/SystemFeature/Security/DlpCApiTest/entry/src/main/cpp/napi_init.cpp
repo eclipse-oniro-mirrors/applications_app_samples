1c27100d7d94af4fbf99fb37cbc8c3e64c95ac3a
gcw_RYdx6Zzw <zhongjie35@huawei.com>
2026-08-14 10:30:25 +0800

    update: 更新文件 napi_init.cpp
    Signed-off-by: gcw_RYdx6Zzw <zhongjie35@huawei.com>

--- code/SystemFeature/Security/DlpCApiTest/entry/src/main/cpp/napi_init.cpp
+++ code/SystemFeature/Security/DlpCApiTest/entry/src/main/cpp/napi_init.cpp
@@ -23,8 +23,8 @@
 // [Start dlp_C_GetDlpPermissionInfo]
 static napi_value GetDlpPermissionInfo(napi_env env, napi_callback_info info)
 {
-    DLP_FileAccess dlpFileAccess = NO_PERMISSION; //表示DLP文件授权类型
-    uint32_t flags = 0; //表示DLP文件的详细操作权限
+    DLP_FileAccess dlpFileAccess = NO_PERMISSION; // 表示DLP文件授权类型
+    uint32_t flags = 0; // 表示DLP文件的详细操作权限
     DLP_ErrCode ret = OH_DLP_GetDlpPermissionInfo(&dlpFileAccess, &flags);
     if (ret == DLP_ErrCode::ERR_OH_SUCCESS) {
         napi_value result[2] = {nullptr};
@@ -41,8 +41,8 @@ static napi_value GetDlpPermissionInfo(napi_env env, napi_callback_info info)
 // [Start dlp_C_GetOriginalFileName]
 static napi_value GetOriginalFileName(napi_env env, napi_callback_info info)
 {
-    const char *fileName = "test.txt.dlp"; //表示DLP文件名，用以获取原始文件名
-    char *originalFileName = nullptr; //表示原始文件名
+    const char *fileName = "test.txt.dlp"; // 表示DLP文件名，用以获取原始文件名
+    char *originalFileName = nullptr; // 表示原始文件名
     DLP_ErrCode ret = OH_DLP_GetOriginalFileName(fileName, &originalFileName);
     if (ret == DLP_ErrCode::ERR_OH_SUCCESS) {
         napi_value result = nullptr;
@@ -51,7 +51,7 @@ static napi_value GetOriginalFileName(napi_env env, napi_callback_info info)
     }
     napi_value result = nullptr;
     napi_create_int32(env, ret, &result);
-    free(originalFileName); //处理完后手动释放originalFileName
+    free(originalFileName); // 处理完后手动释放originalFileName
     return result;
 }
 // [End dlp_C_GetOriginalFileName]
@@ -59,7 +59,7 @@ static napi_value GetOriginalFileName(napi_env env, napi_callback_info info)
 // [Start dlp_C_IsInSandbox]
 static napi_value IsInSandbox(napi_env env, napi_callback_info info)
 {
-    bool isInSandbox = false; //true表示当前应用在沙箱中，false表示应用不在沙箱
+    bool isInSandbox = false; // true表示当前应用在沙箱中，false表示应用不在沙箱
     DLP_ErrCode ret = OH_DLP_IsInSandbox(&isInSandbox);
     if (ret == DLP_ErrCode::ERR_OH_SUCCESS) {
         napi_value result = nullptr;
@@ -75,7 +75,7 @@ static napi_value IsInSandbox(napi_env env, napi_callback_info info)
 // [Start dlp_C_SetSandboxAppConfig]
 static napi_value SetSandboxAppConfig(napi_env env, napi_callback_info info)
 {
-    const char *configInfo = "configInfo"; //沙箱应用配置信息，用户可将配置信息json化后传入
+    const char *configInfo = "configInfo"; // 沙箱应用配置信息，用户可将配置信息json化后传入
     DLP_ErrCode ret = OH_DLP_SetSandboxAppConfig(configInfo);
     
     napi_value result = nullptr;
@@ -87,7 +87,7 @@ static napi_value SetSandboxAppConfig(napi_env env, napi_callback_info info)
 // [Start dlp_C_GetSandboxAppConfig]
 static napi_value GetSandboxAppConfig(napi_env env, napi_callback_info info)
 {
-    char *configInfo = nullptr; //输出json化后的沙箱应用配置信息
+    char *configInfo = nullptr; // 输出json化后的沙箱应用配置信息
     DLP_ErrCode ret = OH_DLP_GetSandboxAppConfig(&configInfo);
     if (ret == DLP_ErrCode::ERR_OH_SUCCESS) {
         napi_value result = nullptr;
@@ -96,7 +96,7 @@ static napi_value GetSandboxAppConfig(napi_env env, napi_callback_info info)
     }
     napi_value result = nullptr;
     napi_create_int32(env, ret, &result);
-    free(configInfo); //处理完后手动释放configInfo
+    free(configInfo); // 处理完后手动释放configInfo
     return result;
 }
 // [End dlp_C_GetSandboxAppConfig]
